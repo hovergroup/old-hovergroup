bf77825759901a703863565b70fa022d137817e6
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2012-06-19 06:05:30 +0000

    driver binary data handling, range pulses

--- trunk/ivp-extend/trunk/src/iacomms_driver/acomms_driver.h
+++ trunk/ivp-extend/trunk/src/iacomms_driver/acomms_driver.h
@@ -13,6 +13,7 @@
 #include "goby/util/binary.h"
 #include "goby/common/logger.h"
 #include "goby/acomms/connect.h"
+#include "XYRangePulse.h"
 
 class acomms_driver : public CMOOSApp
 {
@@ -29,6 +30,10 @@ protected:
 	google::protobuf::uint32 my_id;
 	std::string port_name, my_name;
 	bool use_psk_for_minipackets;
+	double m_navx, m_navy;
+
+	double transmission_pulse_range, transmission_pulse_duration, receive_pulse_range,
+		receive_pulse_duration;
 
 	int transmission_rate, transmission_dest;
 	std::string transmission_data;
@@ -36,7 +41,7 @@ protected:
 	goby::acomms::ModemDriverBase* driver;
 	goby::acomms::protobuf::DriverConfig cfg;
 
-	void transmit_data( bool isBinary );
+	void transmit_data();
 	void handle_data_receive( const goby::acomms::protobuf::ModemTransmission& data_msg );
 	void publishReceivedInfo( goby::acomms::protobuf::ModemTransmission trans, int index );
 	void handle_raw_incoming( const goby::acomms::protobuf::ModemRaw& msg );
@@ -49,6 +54,7 @@ protected:
 
 	void publishWarning( std::string message );
 	void publishStatus( std::string status_update );
+	void postRangePulse( std::string label, double range, double duration, std::string color = "yellow" );
 
 	void RegisterVariables();
 
