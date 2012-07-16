2402d4af2239a151e316674bbda1a1d1af38d480
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2012-07-16 09:51:50 +0000

    minor update to acomms_driver to properly handle unhandled rates ongoing development to parser

--- ivp-extend/trunk/src/acomms_alog_parser/acomms_alog_parser.cpp
+++ ivp-extend/trunk/src/acomms_alog_parser/acomms_alog_parser.cpp
@@ -11,7 +11,38 @@ using namespace std;
 using namespace boost::posix_time;
 using namespace lib_acomms_messages;
 
+#define COLOR_YELLOW "\33[33m"
+#define COLOR_RED "\33[31m"
+#define COLOR_RESET "\33[0m"
+
 ACOMMS_ALOG_PARSER::ACOMMS_ALOG_PARSER() {
+}
+
+pair<int,double> findNearest( vector<double> time_vector, double msg_time ) {
+	if ( time_vector.empty() ) return pair<int,double>(-1,-1);
+	double min_diff = 10000;
+	int index = -1;
+	for ( int i=0; i<time_vector.size(); i++ ) {
+		if ( fabs(time_vector[i]-msg_time) < min_diff ) {
+			min_diff = fabs(time_vector[i]-msg_time);
+			index = i;
+		}
+	}
+	return pair<int,double>(index,min_diff);
+}
+
+template <class T>
+pair<int,double> findNearest( vector< pair<double,T> > item_list, double msg_time ) {
+	if ( item_list.empty() ) return pair<int,double>(-1,-1);
+	double min_diff = 10000;
+	int index = -1;
+	for ( int i=0; i<item_list.size(); i++ ) {
+		if ( fabs(item_list[i].first-msg_time) < min_diff ) {
+			min_diff = fabs(item_list[i].first-msg_time);
+			index = i;
+		}
+	}
+	return pair<int,double>(index,min_diff);
 }
 
 bool transmission_sort ( ACOMMS_ALOG_PARSER::TRANSMISSION_EVENT t1, ACOMMS_ALOG_PARSER::TRANSMISSION_EVENT t2 ) {
@@ -24,40 +55,52 @@ bool reception_sort ( ACOMMS_ALOG_PARSER::RECEPTION_EVENT r1, ACOMMS_ALOG_PARSER
 
 void ACOMMS_ALOG_PARSER::runParser() {
 	for ( int i=0; i<alog_files.size(); i++ ) {
+		// perform intial processing
+		// gets header lines, vehicle name, acomms ID, time offset, data, etc.
 		alog_files[i].processFile();
+
+		// get time offset of our log file
 		double time_offset = alog_files[i].getMOOSTimeOffset();
 
+		// add receipts to our list of receptions for this vehicle, now with absolute time
 		for ( int j=0; j<alog_files[i].receptions.size(); j++ ) {
 			RECEPTION_EVENT r_event = alog_files[i].receptions[j].second;
 			r_event.receive_time += time_offset;
 			r_event.receive_start_time += time_offset;
 			all_receptions[alog_files[i].getVehicleName()].push_back( r_event );
 		}
+
+		// add transmissions to our list of all recptions, now with absolute time
 		for ( int j=0; j<alog_files[i].transmissions.size(); j++ ) {
 			TRANSMISSION_EVENT t_event = alog_files[i].transmissions[j].second;
 			t_event.transmission_time += time_offset;
 			all_transmissions.push_back( t_event );
 		}
 
+		// add vehicle name to list of all vehicles if not already found
 		if ( find( vehicle_names.begin(), vehicle_names.end(), alog_files[i].getVehicleName() ) == vehicle_names.end() ) {
 			vehicle_names.push_back( alog_files[i].getVehicleName() );
 		}
 	}
 
+	// for each vehicle, sort receptions by time
 	for ( int i=0; i<vehicle_names.size(); i++ ) {
 		sort( all_receptions[vehicle_names[i]].begin(),
 				all_receptions[vehicle_names[i]].end(), reception_sort );
 	}
 
+	// sort all transmissions by time
 	sort( all_transmissions.begin(), all_transmissions.end(), transmission_sort );
 
-
-
+	// count up total number of receptions
 	int sum = 0;
 	for ( int i=0; i<vehicle_names.size(); i++ ) {
 		sum+=all_receptions[vehicle_names[i]].size();
 	}
-	cout << sum << " available receptions" << endl;
+	cout << sum << " receptions" << endl;
+	cout << all_transmissions.size() << " transmissions" << endl;
+
+	return;
 
 	int sync_losses = 0;
 	for ( int i=0; i<all_transmissions.size(); i++ ) {
@@ -155,16 +198,19 @@ void ACOMMS_ALOG_PARSER::runParser() {
 
 // ------------------------------------------------------------------------------------------------
 
+// intiial processing of an alog file
 void ACOMMS_ALOG_PARSER::FILE_INFO::processFile() {
-	cout << filename << endl;
+	cout << "Entering file: " << filename << endl;
 
 //	cout << "opening " << filename << endl;
 	openFile();
 
 //	cout << "parsing moos file" << endl;
+	// parse the associated moos file
 	parseMOOSFile();
 //	cout << "vehicle: " << vehicle_name << endl;
 //	cout << "getting header lines" << endl;
+	// parse header lines
 	parseHeaderLines();
 //	cout << "collecting data" << endl;
 	collectData();
@@ -172,27 +218,47 @@ void ACOMMS_ALOG_PARSER::FILE_INFO::processFile() {
 //	cout << "closing file" << endl;
 	closeFile();
 
+	// determine the time offset of this file
 	if ( !offsetViaGPS() ) {
 		cout << "Failed to offset using gps, using system time from header." << endl;
 		offsetViaHeader();
 	}
 
-	cout << "vehicle: " << vehicle_name << "  transmissions: " << transmissions.size() << "  receptions: " << receptions.size() << endl << endl;
+	// print some basic info about this log file
+	cout << "vehicle: " << vehicle_name << "  transmissions: "
+			<< transmissions.size() << "  receptions: " << receptions.size()
+			<< endl << endl;
 }
 
+// force an alog entry to be a string
+string fixToString( ALogEntry entry, int line_num ) {
+	if ( entry.isNumerical() ) {
+		cout << COLOR_YELLOW << "WARNING at line " << line_num <<
+				": " << entry.getVarName() << " was numerical, will convert." << COLOR_RESET << endl;
+		stringstream ss;
+		ss << entry.getDoubleVal();
+		return ss.str();
+	} else {
+		return entry.getStringVal();
+	}
+}
+
+// main data collection function, all relevant variables need to picked up here
 void ACOMMS_ALOG_PARSER::FILE_INFO::collectData() {
-//	cout << "fetching first" << endl;
 	ALogEntry entry = getNextEntry();
-//	cout << "got first" << endl;
 	int iteration = 0;
+
+	// iterate over the entire file
 	while ( entry.getStatus() != "eof" ) {
-//		iteration++;
+		iteration++;
+		int line_num = iteration+5; // 5 header lines
 //		if ( iteration%1000 == 0 ) {
 //			cout << entry.getTimeStamp() << endl;
 //		}
 		if ( entry.getStatus() != "invalid" ) {
 			string key = entry.getVarName();
 			double msg_time = entry.getTimeStamp();
+
 			if ( key == "GPS_X" || key == "NAV_X" ) {
 				gps_x.push_back( pair<double,double>( msg_time, entry.getDoubleVal() ) );
 			} else if ( key == "GPS_Y" || key == "NAV_Y" ) {
@@ -209,67 +275,194 @@ void ACOMMS_ALOG_PARSER::FILE_INFO::collectData() {
 			} else if ( key == "ACOMMS_DRIVER_STATUS" ) {
 				acomms_driver_status.push_back( pair<double,string>( msg_time, entry.getStringVal() ) );
 			} else if ( key == "ACOMMS_TRANSMIT_DATA" ) {
-				acomms_transmit_data.push_back( pair<double,string>( msg_time, entry.getStringVal() ) );
+				acomms_transmit_data.push_back( pair<double,string>( msg_time,
+						fixToString( entry, line_num) ) );
 			} else if ( key == "ACOMMS_RECEIVED_ALL" ) {
+//				unprocessed_receipts.push_back( pair<double,string>( iteration, entry.getStringVal() ) );
 				receptions.push_back( pair<double, RECEPTION_EVENT>( msg_time,
-						constructReception( msg_time, entry.getStringVal() ) ) );
+						constructReception( msg_time, line_num, entry.getStringVal() ) ) );
 			} else if ( key == "ACOMMS_TRANSMIT_SIMPLE" ) {
+//				unprocessed_transmissions.push_back( pair<double,string>( line_num, entry.getStringVal() ) );
 				transmissions.push_back( pair<double, TRANSMISSION_EVENT>( msg_time,
-						constructTransmission( msg_time, entry.getStringVal() ) ) );
+						constructTransmission( msg_time, line_num, entry.getStringVal() ) ) );
+			} else if ( key == "VOLTAGE" ) {
+				voltage.push_back( pair<double,double>( msg_time, entry.getDoubleVal() ) );
+			} else if ( key == "ACOMMS_TRANSMITTED_DATA_HEX" ) {
+				acomms_transmitted_data_hex.push_back( pair<double,string>( msg_time,
+						fixToString( entry, line_num ) ) );
+			} else if ( key == "ACOMMS_RECEIVED_DATA_HEX" ) {
+				acomms_received_data_hex.push_back( pair<double,string>( msg_time,
+						fixToString( entry, line_num ) ) );
 			}
 		}
 
 		entry = getNextEntry();
 	}
+
+	// finish constructing receptions
+	// check for no receive hex data in log file
+	if ( acomms_received_data_hex.empty() && !receptions.empty() ) {
+		cout << COLOR_YELLOW <<
+				"WARNING: this log file does not appear to contain hex formatted receive data"
+				<< COLOR_RESET << endl;
+	} else {
+		for ( int i=0; i<receptions.size(); i++ ) {
+			// if there are frames, look for hex posting
+			if ( receptions[i].second.data_msg.frame_size() > 0 ) {
+				// find the hex data nearest to our time
+				pair<int,double> hex_index = findNearest( acomms_received_data_hex, receptions[i].first );
+				if ( hex_index.first == -1 ) { // failed to find
+					cout << COLOR_YELLOW << "WARNING at line " << receptions[i].second.source_line
+							<< ": failed to find hex data for reception." << COLOR_RESET << endl;
+				} else if ( hex_index.second > 1 ) { // time not very close
+						cout << COLOR_YELLOW << "WARNING at line " << receptions[i].second.source_line
+								<< ": found receive hex data " << hex_index.second
+								<< " seconds away - not setting." << COLOR_RESET << endl;
+				} else { // all good
+					receptions[i].second.received_data_hex =
+							acomms_received_data_hex[hex_index.first].second;
+				}
+			}
+		}
+	}
+
+	// finish constructing transmissions
+	// check for no transmit hex data in log file
+	if ( acomms_transmitted_data_hex.empty() && !transmissions.empty() ) {
+		cout << COLOR_YELLOW <<
+				"WARNING: this log file does not appear to contain hex formatted transmit data"
+				<< COLOR_RESET << endl;
+	} else {
+		for ( int i=0; i<transmissions.size(); i++ ) {
+			pair<int,double> hex_index = findNearest( acomms_transmitted_data_hex, transmissions[i].first );
+			if ( hex_index.first == -1 ) { // failed to find
+				cout << COLOR_YELLOW << "WARNING at line " << transmissions[i].second.source_line
+						<< ": failed to find hex data for transmission." << COLOR_RESET << endl;
+			} else if ( hex_index.second > 1 ) { // time not very close
+				cout << COLOR_YELLOW << "WARNING at line " << transmissions[i].second.source_line
+						<< ": found transmit hex data " << hex_index.second
+						<< " seconds away - deleting transmission (perhaps unhandled rate?)." << COLOR_RESET << endl;
+				transmissions.erase( transmissions.begin()+=i );
+			} else { // all good
+				transmissions[i].second.transmitted_data_hex =
+						acomms_transmitted_data_hex[hex_index.first].second;
+			}
+		}
+	}
 }
 
-ACOMMS_ALOG_PARSER::RECEPTION_EVENT ACOMMS_ALOG_PARSER::FILE_INFO::constructReception( double msg_time, string msg_val ) {
+// construct a reception event from a ACOMMS_RECEIVED_ALL log entry
+ACOMMS_ALOG_PARSER::RECEPTION_EVENT ACOMMS_ALOG_PARSER::FILE_INFO::constructReception( double msg_time, int line_number, string msg_val ) {
 //	cout << msg_time << endl;
 //	cout << msg_val << endl;
 	RECEPTION_EVENT r_event;
+
+	// file information
+	r_event.source_filename = filename;
+	r_event.source_line = line_number;
+
+	// vehicle information
 	r_event.vehicle_name = vehicle_name;
 	r_event.vehicle_id = vehicle_id;
+
+	// time of message
 	r_event.receive_time = msg_time;
 
+	// reconstruct the protobuf
+	// check for bad time in receive stats
+	if ( msg_val.find("time: \"") != string::npos ) {
+		cout << COLOR_YELLOW << "WARNING at line " << line_number <<
+				": bad time in received_all - attempting to correct" << COLOR_RESET << endl;
+		// position of bad time
+		int bad_time_index = msg_val.find("time: \"");
+		int bad_time_end_index = msg_val.find("<|>",bad_time_index)-1;
+
+		// position of good time
+		int good_time_index = msg_val.find("time: ");
+		int good_time_end_index = msg_val.find("<|>",good_time_index)-1;
+
+		// make switch
+		string good_time = msg_val.substr(good_time_index, good_time_end_index-good_time_index+1);
+		msg_val.replace(bad_time_index, bad_time_end_index-bad_time_index+1,
+				good_time);
+	}
+	// put line endings back in
 	while ( msg_val.find("<|>") != string::npos ) {
 		msg_val.replace( msg_val.find("<|>"), 3, "\n" );
 	}
 	string const* ptr = &msg_val;
-	google::protobuf::TextFormat::ParseFromString( *ptr, &r_event.data_msg );
+	if ( !google::protobuf::TextFormat::ParseFromString( *ptr, &r_event.data_msg ) ) {
+		cout << COLOR_RED << "ERROR parsing protobuf at line " << line_number
+				<< COLOR_RESET << endl;
+	}
 
+	// most recent gps information
 	if ( gps_x.size() > 0 ) {
 		r_event.gps_x = gps_x.back().second;
 		r_event.gps_y = gps_y.back().second;
 		r_event.gps_age = r_event.receive_time-gps_x.back().first;
 	}
 
+	// backwards search from receive time to find beginning of receipt
 	for ( int i=acomms_driver_status.size()-1; i>=0; i-- ) {
 		if ( acomms_driver_status[i].second == "receiving" ) {
 			r_event.receive_start_time = acomms_driver_status[i].first;
 			break;
 		}
 	}
+	if ( msg_time - r_event.receive_start_time > 7 ) {
+		cout << COLOR_YELLOW << "WARNING at line " << line_number <<
+			": receive start time " << msg_time-r_event.receive_start_time <<
+			" seconds before receive complete time." << COLOR_RESET << endl;
+	}
 
 //	cout << "parsed okay" << endl;
 	return r_event;
 }
 
-ACOMMS_ALOG_PARSER::TRANSMISSION_EVENT ACOMMS_ALOG_PARSER::FILE_INFO::constructTransmission( double msg_time, string msg_val ) {
+ACOMMS_ALOG_PARSER::TRANSMISSION_EVENT ACOMMS_ALOG_PARSER::FILE_INFO::constructTransmission( double msg_time, int line_number, string msg_val ) {
 //	cout << msg_time << endl;
 //	cout << msg_val << endl;
 	TRANSMISSION_EVENT t_event;
+
+	// file information
+	t_event.source_filename = filename;
+	t_event.source_line = line_number;
+
+	// vehicle info
 	t_event.transmitter_name = vehicle_name;
 	t_event.source_id = vehicle_id;
+
+	// time of message
 	t_event.transmission_time = msg_time;
+
+	// look for most recent gps info
 	if ( gps_x.size() > 0 ) {
 		t_event.gps_x = gps_x.back().second;
 		t_event.gps_y = gps_y.back().second;
 		t_event.gps_age = t_event.transmission_time - gps_x.back().first;
 	}
 
-	if ( acomms_transmit_data.size()!=0 )
+	// look for data that was transmitted
+	if ( acomms_transmit_data.size()!=0) {
 		t_event.data = acomms_transmit_data.back().second;
+		// check time of data
+		if ( msg_time - acomms_transmit_data.back().first > 5 ) {
+			cout << COLOR_YELLOW << "WARNING at line " << line_number <<
+					": transmit data " << msg_time-acomms_transmit_data.back().first <<
+					" seconds old." << COLOR_RESET << endl;
+		}
+	}
+	// check for overlapping data transmissions
+	if ( acomms_transmit_data.size() > 1 ) {
+		if ( acomms_transmit_data.back().first -
+				acomms_transmit_data[acomms_transmit_data.size()-2].first < 5 ) {
+			cout << COLOR_YELLOW << "WARNING at line " << line_number <<
+					": two transmit data's within 5 seconds of each other." << COLOR_RESET << endl;
+		}
+	}
 
+	// parse transmit info from msg for rate and destination
 	SIMPLIFIED_TRANSMIT_INFO ats( msg_val );
 	t_event.rate = ats.rate;
 	t_event.destination_id = ats.dest;
@@ -288,6 +481,7 @@ void ACOMMS_ALOG_PARSER::FILE_INFO::openFile() {
 }
 
 void ACOMMS_ALOG_PARSER::FILE_INFO::parseHeaderLines() {
+	// pull first 5 lines from logfile (middle 3 are headers)
 	getNextLine();
 	header_lines.clear();
 	for ( int i=0; i<3; i++ )
@@ -313,32 +507,22 @@ string ACOMMS_ALOG_PARSER::FILE_INFO::getNextLine() {
 }
 
 void ACOMMS_ALOG_PARSER::FILE_INFO::parseMOOSFile() {
+	// find the moos file by replacing file extension
 	string moosfilename = filename;
 	int extension_index = moosfilename.find(".alog");
 	moosfilename.replace(extension_index, 5, "._moos");
 
-//	FILE * this_moos_file = fopen( moosfilename.c_str(), "r");
-//	string line = getNextRawLine( this_moos_file );
-//	while ( line != "eof" ) {
-//		// needs improvement
-//		if ( (line.find("community") != string::npos || line.find("Community") != string::npos)
-//				 && (line.find("//") == string::npos || line.find("//") > 10 ) ) {
-//			int tmp_index = line.find("=")+1;
-//			vehicle_name = stripBlankEnds( line.substr(tmp_index, line.size()-tmp_index) );
-//			break;
-//		}
-//		line = getNextRawLine( this_moos_file );
-//	}
-//
-//	fclose( this_moos_file );
-
+	// construct a mission reader to read our moos file
 	CProcessConfigReader missionreader;
 	missionreader.SetFile( moosfilename );
+
+	// get acomms ID and vehicle name (community name)
 	missionreader.GetConfigurationParam("iacomms_driver", "ID", vehicle_id);
 	missionreader.GetValue("Community", vehicle_name);
 }
 
 void ACOMMS_ALOG_PARSER::FILE_INFO::offsetViaHeader() {
+	// parse date out of the header
 	int date_start = header_lines[1].find("ON ") + 3;
 	string date_string = header_lines[1].substr(date_start, header_lines[1].size()-date_start);
 	if ( date_string[0] == ' ' )
@@ -351,6 +535,7 @@ void ACOMMS_ALOG_PARSER::FILE_INFO::offsetViaHeader() {
 //	cout << boost::posix_time::to_simple_string( creation_time ) << endl;
 
 //	ALogEntry entry = getNextRawALogEntry( logfile );
+	// convert to seconds (since 12am)
 	moos_time_offset = creation_time.time_of_day().total_milliseconds()/1000.0;// - entry.time();
 }
 
@@ -358,18 +543,24 @@ bool ACOMMS_ALOG_PARSER::FILE_INFO::offsetViaGPS() {
 	if ( gps_time.size() < 2 ) {
 		return false;
 	} else {
+		// parse ptime and apply time zone correction
 		ptime first_gps_ptime = gps_time[1].second;
 		time_duration utc_correction = hours(UTC_TIME_OFFSET);
 		first_gps_ptime += utc_correction;
+
+		// time at which that gps time was received, seconds and milliseconds
 		int first_gps_moos_time_seconds = floor( gps_time[1].first );
 		int first_gps_moos_time_milliseconds = floor( gps_time[1].first*1000.0) - first_gps_moos_time_seconds*1000;
+		// convert to time duration
 		time_duration first_gps_td = seconds ( first_gps_moos_time_seconds ) +
 				milliseconds( first_gps_moos_time_milliseconds );
 
-//			cout << "first gps time " << first_gps_td << endl;
-//			cout << "ptime " << first_gps_ptime << endl;
+//		cout << "first gps time " << first_gps_td << endl;
+//		cout << "ptime " << first_gps_ptime << endl;
 
+		// time 0 in log file = this gps time - time since 0
 		creation_time = first_gps_ptime - first_gps_td;// - first_moos_time_td;
+		// convert to seconds - time since 12am that day
 		moos_time_offset = creation_time.time_of_day().total_milliseconds()/1000.0;// -first_moos_time;
 		return true;
 	}
@@ -382,7 +573,7 @@ void ACOMMS_ALOG_PARSER::addAlogFile( std::string filename ) {
 }
 
 void ACOMMS_ALOG_PARSER::addAlogFile( boost::filesystem::path filepath ) {
-//	NEVER LOOK HERE
+	// AVERT THINE EYES
 	alog_files.push_back( FILE_INFO( string( filepath.string().c_str() ) ) );
 }
 
--- ivp-extend/trunk/src/acomms_alog_parser/acomms_alog_parser_main.cpp
+++ ivp-extend/trunk/src/acomms_alog_parser/acomms_alog_parser_main.cpp
@@ -21,27 +21,29 @@ vector<fs::path> alog_files;
 void searchDirectory ( fs::path directory_path, int depth );
 
 int main(int argc, char *argv[]) {
+	// define the path where we look for alog files
 	fs::path full_path( fs::initial_path<fs::path>() );
-
 	if ( argc > 1 ) {
 		full_path = fs::system_complete( fs::path( argv[argc-1] ) );
 	} else {
 		cout << "Must specify a directory." << endl;
 		return 1;
 	}
-
+	// check that specified folder exists
 	if ( !fs::exists( full_path ) ) {
 		cout << "Directory not found: " << full_path << endl;
 		return 1;
 	}
-
+	// check that specified path is a directory and not a file
 	if ( !fs::is_directory( full_path ) ) {
 		cout << full_path << " is not a directory." << endl;
 		return 1;
 	}
 
+	// look for alog files in the specified directory
 	searchDirectory ( full_path, 0 );
 
+	// construct our parser and pass it alog files
 	ACOMMS_ALOG_PARSER parser;
 	cout << "Found " << alog_files.size() << " alog files." << endl;
 	for ( int i=0; i<alog_files.size(); i++ ) {
@@ -49,23 +51,27 @@ int main(int argc, char *argv[]) {
 		parser.addAlogFile( alog_files[i] );
 	}
 
+	// run the parser
 	parser.runParser();
 
 	return 0;
 }
 
 void searchDirectory ( fs::path directory_path, int depth ) {
-	if ( depth > 2 ) return;
+	if ( depth > 2 ) return; // maximum search depth
 
+	// iterate over items in the directory
 	fs::directory_iterator end_iter;
 	for ( fs::directory_iterator dir_itr( directory_path );
 			dir_itr != end_iter; ++dir_itr ) {
 
 		try {
+			// if file, check file extension
 			if ( fs::is_regular_file( dir_itr->status() ) ) {
 				if ( fs::extension( dir_itr->path().filename() ) == ".alog" ) {
 					alog_files.push_back( fs::system_complete( dir_itr->path() ) );
 				}
+			// if directory, search in that directory after incrementing depth
 			} else if ( fs::is_directory( dir_itr->status() ) )
 				searchDirectory( fs::system_complete( dir_itr->path() ), depth+1 );
 		} catch ( const exception & ex ) {
