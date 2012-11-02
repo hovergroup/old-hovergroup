eba565a132d522ac1e210ddbab09e6c4ae8ea84a
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2012-11-02 23:08:56 +0000

    findNearestSlot function defined

--- ivp-extend/josh/src/lib_timed-acomms/TimedAcomms.h
+++ ivp-extend/josh/src/lib_timed-acomms/TimedAcomms.h
@@ -14,77 +14,77 @@
 
 class TimedAcomms {
 public:
-	TimedAcomms();
+    TimedAcomms();
 
-	void doWork();
+    void doWork();
 
-	// data inputs
-	bool processGpsTimeSeconds( double gps_time_seconds );
-	void signalStartOfModemReceiving();
-	void signalBadReception();
-	void signalGoodReception( std::string data );
+    // data inputs
+    bool processGpsTimeSeconds(double gps_time_seconds);
+    void signalStartOfModemReceiving();
+    void signalBadReception();
+    void signalGoodReception(std::string data);
 
-	// output signals - use goby methods to connect
-	boost::signals2::signal<void ()> signal_no_receipt;
-	boost::signals2::signal<void (const std::string data)> signal_receipt;
-	boost::signals2::signal<void ()> signal_transmit;
-	boost::signals2::signal<void (const std::string msg)> signal_debug, signal_updates;
+    // output signals - use goby methods to connect
+    boost::signals2::signal<void()> signal_no_receipt;
+    boost::signals2::signal<void(const std::string data)> signal_receipt;
+    boost::signals2::signal<void()> signal_transmit;
+    boost::signals2::signal<void(const std::string msg)> signal_debug,
+            signal_updates;
 
-	// configuration
-	void setReceiveTiming( double period, double offset );
-	void setTransmitTiming( double period, double offset );
-	void setReceivingExtension( double extension );
-
+    // configuration
+    void setReceiveTiming(double period, double offset);
+    void setTransmitTiming(double period, double offset);
+    void setReceivingExtension(double extension);
 
 private:
-	static const int GPS_TIME_SAMPLES=5;
+    static const int GPS_TIME_SAMPLES = 5;
 
-	enum StateIDs { GPS_TIME_CAL=0,
-					EXITING_TIME_CAL,
-					READY,
-					RECEIVING };
+    enum StateIDs {
+        GPS_TIME_CAL = 0, EXITING_TIME_CAL, READY, RECEIVING
+    };
 
-	StateIDs m_State;
-	std::map<StateIDs, std::string> m_StateNames;
+    StateIDs m_State;
+    std::map<StateIDs, std::string> m_StateNames;
 
-	double getSysTimeSeconds(); // system clock
-	double getAbsTimeSeconds(); // system clock with gps correction
+    double getSysTimeSeconds(); // system clock
+    double getAbsTimeSeconds(); // system clock with gps correction
 
-	// clock adjustment
-	double m_ClockOffset;
-	double m_ClockErrorSum;
-	int m_ClockSamples;
+    // clock adjustment
+    double m_ClockOffset;
+    double m_ClockErrorSum;
+    int m_ClockSamples;
 
-	// timing configuration
-	double m_TransmitPeriod, m_TransmitOffset;
-	double m_ReceivePeriod, m_ReceiveOffset;
-	double m_AllowedReceivingExtension;
+    // timing configuration
+    double m_TransmitPeriod, m_TransmitOffset;
+    double m_ReceivePeriod, m_ReceiveOffset;
+    double m_AllowedReceivingExtension;
 
-	// timing operations
-	int findNextSlot( double current_time, double period, double offset );
-	int findPreviousSlot( double current_time, double period, double offset );
-	double slot2Time( int slot, double period, double offset );
+    // timing operations
+    int findNextSlot(double current_time, double period, double offset);
+    int findPreviousSlot(double current_time, double period, double offset);
+    int findNearestSlot(double current_time, double period, double offset);
+    double slot2Time(int slot, double period, double offset);
 
-	// state methods
-	void doReadyState();
-	void doExitingTimeCalState();
-	void doGpsTimeCalState();
-	void doReceivingState();
+    // state methods
+    void doReadyState();
+    void doExitingTimeCalState();
+    void doGpsTimeCalState();
+    void doReceivingState();
 
-	// transmission
-	int m_LastTransmitSlot;
+    // transmission
+    int m_LastTransmitSlot;
 
-	// receiving
-	int m_LastReceiveSlot;
-	int m_ExpectedReceiveSlot;
+    // receiving
+    int m_LastReceiveSlot;
+    int m_ExpectedReceiveSlot;
 
-	// persistent data
-	int m_ExitingTimeCalIterations;
-	double m_ThisRunTime, m_LastRunTime;
+    // persistent data
+    int m_ExitingTimeCalIterations;
+    double m_ThisRunTime, m_LastRunTime;
 
-	// incoming data
-	bool m_BeginModemReceive, m_BadReceive, m_GoodReceive;
-	std::string m_ReceivedData;
+    // incoming data
+    bool m_BeginModemReceive, m_BadReceive, m_GoodReceive;
+    std::string m_ReceivedData;
 };
 
 #endif /* TIMEDACOMMS_H_ */
