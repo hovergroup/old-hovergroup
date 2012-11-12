6db8efbfe37a67e16319cdc9a083934860db9795
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2012-11-12 06:52:51 +0000

    added acclerating circle simulation type

--- trunk/ivp-extend/josh/src/lib_TargetSim/TargetSim.cpp
+++ trunk/ivp-extend/josh/src/lib_TargetSim/TargetSim.cpp
@@ -17,6 +17,8 @@ void BaseSim::getTargetPos(double & x, double & y) {
 	y = m_osy;
 }
 
+// --------------------------------------------------------------
+
 void CircleSim::setConfiguration(CProcessConfigReader & m_MissionReader) {
 	m_MissionReader.GetConfigurationParam("center_x", m_centerX);
 	m_MissionReader.GetConfigurationParam("center_y", m_centerY);
@@ -77,6 +79,78 @@ void CircleSim::doWork(double time) {
 
 	double timeDiff = m_thisRunTime - m_lastRunTime;
 	double angleDiff = timeDiff * m_angularSpeed;
+	if (m_clockwise)
+		m_currentAngle += angleDiff;
+	else
+		m_currentAngle -= angleDiff;
+	while ( m_currentAngle > 360 ) m_currentAngle -= 360.0;
+	while ( m_currentAngle < -360 ) m_currentAngle += 360.0;
+
+	setPos( m_currentAngle );
+}
+
+// --------------------------------------------------------------
+
+void AcceleratingCircleSim::reset() {
+	m_state = Paused;
+	m_currentVelocity = m_initialVelocity;
+	initialPos();
+}
+
+/**
+ * acceleration can be negative or positive, units of m/s^2
+ * if negative, max_speed will be treated as minimum speed
+ */
+void AcceleratingCircleSim::setConfiguration(CProcessConfigReader & m_MissionReader) {
+	m_MissionReader.GetConfigurationParam("center_x", m_centerX);
+	m_MissionReader.GetConfigurationParam("center_y", m_centerY);
+	m_MissionReader.GetConfigurationParam("radius", m_radius);
+	double speed;
+	m_MissionReader.GetConfigurationParam("intial_speed", speed);
+	m_initialVelocity = speed/m_radius * 180.0/M_PI;
+	m_MissionReader.GetConfigurationParam("max_speed", speed);
+	m_maxVelocity = speed/m_radius * 180.0/M_PI;
+	m_MissionReader.GetConfigurationParam("acceleration", speed);
+	m_acceleration = speed/m_radius * 180.0/M_PI;
+	m_MissionReader.GetConfigurationParam("initial_angle", m_initial_angle);
+	m_MissionReader.GetConfigurationParam("clockwise", m_clockwise);
+
+//	std::cout << m_centerX << " ";
+//	std::cout << m_centerY << " ";
+//	std::cout << m_radius << " ";
+//	std::cout << m_angularSpeed << " ";
+//	std::cout << m_clockwise << std::endl;
+
+	m_currentVelocity = m_initialVelocity;
+	initialPos();
+
+//	std::cout << m_osx << " " << m_osy << std::endl;
+}
+
+void AcceleratingCircleSim::doWork(double time) {
+	m_lastRunTime = m_thisRunTime;
+	m_thisRunTime = time;
+
+	if ( first_iteration ) {
+		first_iteration = false;
+		return;
+	}
+
+//	std::cout << m_currentAngle << std::endl;
+//	std::cout << m_osx << " " << m_osy << std::endl;
+	if ( m_state == Paused ) return;
+//	std::cout << "not paused" << std::endl;
+
+	double timeDiff = m_thisRunTime - m_lastRunTime;
+
+	m_currentVelocity += m_acceleration * timeDiff;
+	if ( m_acceleration > 0 && m_currentVelocity > m_maxVelocity ) {
+		m_currentVelocity = m_maxVelocity;
+	} else if ( m_acceleration < 0 && m_currentVelocity < m_maxVelocity ) {
+		m_currentVelocity = m_maxVelocity;
+	}
+
+	double angleDiff = timeDiff * m_currentVelocity;
 	if (m_clockwise)
 		m_currentAngle += angleDiff;
 	else
