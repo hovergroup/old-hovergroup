22a8bc7b9ddf7d99ebfcf0da0c0a50e5d1d249b8
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-08-19 01:50:35 +0000

    switched acomms driver status postings to doubles from string completed draft of pAcommsScheduler

--- trunk/ivp-extend/trunk/src/pAcommsScheduler/AcommsScheduler.cpp
+++ trunk/ivp-extend/trunk/src/pAcommsScheduler/AcommsScheduler.cpp
@@ -13,7 +13,8 @@
 // Constructor
 
 AcommsScheduler::AcommsScheduler() {
-	m_iterations = 0;
+	m_lockEnabled = false;
+	m_state = unset;
 }
 
 //---------------------------------------------------------
@@ -31,6 +32,44 @@ bool AcommsScheduler::OnNewMail(MOOSMSG_LIST &NewMail) {
 	for (p = NewMail.begin(); p != NewMail.end(); p++) {
 		CMOOSMsg &msg = *p;
 		std::string key = msg.GetKey();
+		if (key=="ACOMMS_DRIVER_STATUS") {
+			HoverAcomms::DriverStatus val = static_cast<HoverAcomms::DriverStatus>(msg.GetDouble());
+			if (val==HoverAcomms::RECEIVING) {
+				// start of new receipt
+				m_lastStart = JoshUtil::getSystemTimeSeconds();
+				// reset
+				m_dataComplete = 0;
+			} else if (m_driverStatus==HoverAcomms::RECEIVING && val==HoverAcomms::READY) {
+				// end of receipt
+				m_lastEnd = JoshUtil::getSystemTimeSeconds();
+				// increment
+				m_dataComplete++;
+			}
+			m_driverStatus = val;
+		} else if (key=="ACOMMS_RECEIVED") {
+			HoverAcomms::AcommsReception reception;
+			if (reception.parseFromString(msg.GetString())) {
+				if (reception.getSource()==m_sourceID)
+					m_dataComplete++;
+			}
+		}
+	}
+
+	if (m_dataComplete==2) { // got both status timings and data output
+		m_observations.push_back(std::pair<double,double>(m_lastStart,m_lastEnd));
+		while (m_observations.size()>MAX_OBSERVATIONS) {
+			m_observations.pop_front();
+		}
+		double sum1=0, sum2=0;
+		for (int i=0; i<m_observations.size(); i++) {
+			double slot;
+			sum1 += modf(m_observations[i].first/m_period, &slot);
+			sum2 += m_observations[i].second - m_observations[i].first;
+		}
+		m_meanOffset = sum1/m_observations.size();
+		m_meanDuration = sum2/m_observations.size();
+
+		m_dataComplete=0;
 	}
 
 	return (true);
@@ -43,6 +82,7 @@ bool AcommsScheduler::OnConnectToServer() {
 	m_MissionReader.GetConfigurationParam("period", m_period);
 	m_MissionReader.GetConfigurationParam("pre_lock", m_preLock);
 	m_MissionReader.GetConfigurationParam("post_lock", m_postLock);
+	m_MissionReader.GetConfigurationParam("source_ID", m_sourceID);
 
 	// register for variables here
 	// possibly look at the mission file?
@@ -58,7 +98,49 @@ bool AcommsScheduler::OnConnectToServer() {
 //            happens AppTick times per second
 
 bool AcommsScheduler::Iterate() {
-	m_iterations++;
+	// don't do anything if we don't have at least one observation
+	if (m_observations.size()<1) {
+		setLock(false);
+		return true;
+	}
+
+	// determine current slot and offset
+	double m_time = JoshUtil::getSystemTimeSeconds();
+	double m_offset = modf(m_time/m_period, &m_slot) - m_meanOffset;
+
+	// determine current state within slot
+	if (0<m_offset && m_offset<m_meanDuration) {
+		m_state = lock;
+	} else if (m_meanDuration<m_offset && m_offset<m_meanDuration+m_postLock) {
+		m_state = post_lock;
+	} else if (m_period-m_preLock<m_offset && m_offset<m_period) {
+		m_state = pre_lock;
+	} else {
+		m_state = unlocked;
+	}
+
+	// set lock according to state
+	switch (m_state) {
+	case lock:
+		setLock(true);
+		break;
+
+	case post_lock:
+		setLock(true);
+		break;
+
+	case pre_lock:
+		setLock(true);
+		break;
+
+	case unlocked:
+		setLock(false);
+		break;
+
+	default:
+		break;
+	}
+
 	return (true);
 }
 
@@ -67,7 +149,6 @@ bool AcommsScheduler::Iterate() {
 //            happens before connection is open
 
 bool AcommsScheduler::OnStartUp() {
-
 	RegisterVariables();
 	return (true);
 }
@@ -81,6 +162,16 @@ void AcommsScheduler::RegisterVariables() {
 	m_Comms.Register("ACOMMS_TRANSMIT", 0);
 
 	m_Comms.Register("ACOMMS_RECEIVED", 0);
-	// m_Comms.Register("FOOBAR", 0);
+	m_Comms.Register("ACOMMS_DRIVER_STATUS", 0);
 }
 
+void AcommsScheduler::setLock(bool lock) {
+	// only set if different from current setting
+	if (m_lockEnabled != lock) {
+		if (lock)
+			m_Comms.Notify("ACOMMS_TRANSMIT_LOCKOUT", 1.0);
+		else
+			m_Comms.Notify("ACOMMS_TRANSMIT_LOCKOUT", 0.0);
+	}
+	m_lockEnabled = lock;
+}
--- trunk/ivp-extend/trunk/src/pAcommsScheduler/AcommsScheduler.h
+++ trunk/ivp-extend/trunk/src/pAcommsScheduler/AcommsScheduler.h
@@ -9,6 +9,18 @@
 #define AcommsScheduler_HEADER
 
 #include "MOOS/libMOOS/MOOSLib.h"
+#include "JoshUtils.h"
+#include "HoverAcomms.h"
+#include <math.h>
+#include <deque>
+
+enum STATE {
+	unlocked = 0,
+	pre_lock,
+	lock,
+	post_lock,
+	unset
+};
 
 class AcommsScheduler: public CMOOSApp {
 public:
@@ -23,13 +35,27 @@ protected:
 	void RegisterVariables();
 
 private:
-	// Configuration variables
+	const static double MAX_OBSERVATIONS=4;
 
-private:
-	// State variables
-	unsigned int m_iterations;
-
+	// configuration
 	double m_period, m_preLock, m_postLock;
+	int m_sourceID;
+
+	// state
+	double m_slot;
+	STATE m_state;
+	bool m_lockEnabled;
+
+	void setLock(bool lock);
+
+	// learning
+	HoverAcomms::DriverStatus m_driverStatus;
+	int m_dataComplete;
+
+	double m_lastStart, m_lastEnd; // last observed
+	double m_meanOffset, m_meanDuration; // current mean
+	std::deque<std::pair<double,double> > m_observations; // current history
+
 };
 
 #endif 
