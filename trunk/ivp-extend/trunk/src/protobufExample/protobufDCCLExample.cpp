639fd46f983b0925b511f7613ed9a51631f8ef55
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-09-10 18:22:15 +0000

    worked in protobuf and dccl with examples

---
+++ trunk/ivp-extend/trunk/src/protobufExample/protobufDCCLExample.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "dcclExample.pb.h"
+
+#include "goby/acomms/dccl.h"
+
+int main() {
+	double nav_x = 320.5423;
+	double nav_y = 50.34;
+
+	VehicleStatus vs;
+	VehicleStatusDCCL vsd;
+
+	vs.set_nav_x(nav_x);
+	vs.set_nav_y(nav_y);
+	vs.set_health(VehicleStatus_HealthEnum_HEALTH_GOOD);
+
+	vsd.set_nav_x(nav_x);
+	vsd.set_nav_y(nav_y);
+	vsd.set_health(VehicleStatusDCCL_HealthEnum_HEALTH_GOOD);
+
+	std::cout << "Without dccl: " << vs.SerializeAsString().size() << std::endl;
+	goby::acomms::DCCLCodec* codec = goby::acomms::DCCLCodec::get();
+	try {
+		codec->validate<VehicleStatusDCCL>();
+	} catch (goby::acomms::DCCLException& e) {
+		std::cout << "failed to validate" << std::endl;
+	}
+	std::string bytes;
+	codec->encode(&bytes, vsd);
+	std::cout << "With dccl: " << bytes.size() << std::endl;
+}
