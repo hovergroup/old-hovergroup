306d1894234f5eea67ff5600bba713d74edf8b8d
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-09-11 18:00:19 +0000

    WT working with moos, status view in progress node reports replaced with proto reports in baseline mission

---
+++ trunk/ivp-extend/trunk/src/pWTStatusView/WTStatusView.h
@@ -0,0 +1,52 @@
+/************************************************************/
+/*    NAME:                                               */
+/*    ORGN: MIT                                             */
+/*    FILE: WTStatusView.h                                          */
+/*    DATE:                                                 */
+/************************************************************/
+
+#ifndef WTStatusView_HEADER
+#define WTStatusView_HEADER
+
+#include <Wt/WApplication>
+#include <Wt/WBreak>
+#include <Wt/WContainerWidget>
+#include <Wt/WLineEdit>
+#include <Wt/WPushButton>
+#include <Wt/WText>
+#include <Wt/WTimer>
+#include "MOOS/libMOOS/MOOSLib.h"
+#include "reports.pb.h"
+#include <boost/thread.hpp>
+
+class WTStatusView: public CMOOSApp {
+public:
+	WTStatusView();
+	~WTStatusView();
+
+protected:
+	bool OnNewMail(MOOSMSG_LIST &NewMail);
+	bool Iterate();
+	bool OnConnectToServer();
+	bool OnStartUp();
+	void RegisterVariables();
+
+private:
+	void startWT();
+
+private:
+	boost::thread wtThread;
+
+};
+
+class StatusViewApplication: public Wt::WApplication {
+public:
+	StatusViewApplication(const Wt::WEnvironment& env);
+
+private:
+	void update();
+
+	Wt::WText *voltage_;
+};
+
+#endif 
