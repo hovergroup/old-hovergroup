64a04aef2f9a3123675482ee166339b3b078e237
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-09-27 18:19:43 +0000

    added altimeter driver

---
+++ trunk/ivp-extend/trunk/src/iAltimeter_cruzPro/CMakeFiles/iAltimeter_cruzPro.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/josh/hovergroup/ivp-extend/trunk/src/iAltimeter_cruzPro/Altimeter_cruzPro.cpp" "/home/josh/hovergroup/ivp-extend/trunk/src/iAltimeter_cruzPro/CMakeFiles/iAltimeter_cruzPro.dir/Altimeter_cruzPro.cpp.o"
+  "/home/josh/hovergroup/ivp-extend/trunk/src/iAltimeter_cruzPro/iAltimeter_cruzPro_nmea.cpp" "/home/josh/hovergroup/ivp-extend/trunk/src/iAltimeter_cruzPro/CMakeFiles/iAltimeter_cruzPro.dir/iAltimeter_cruzPro_nmea.cpp.o"
+  "/home/josh/hovergroup/ivp-extend/trunk/src/iAltimeter_cruzPro/main.cpp" "/home/josh/hovergroup/ivp-extend/trunk/src/iAltimeter_cruzPro/CMakeFiles/iAltimeter_cruzPro.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "ASYNCHRONOUS_CLIENT"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
