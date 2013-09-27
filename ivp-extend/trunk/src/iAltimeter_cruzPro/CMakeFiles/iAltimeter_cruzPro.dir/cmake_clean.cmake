312d7afbab9704e59cf5cd6abcd39423f8d21daa
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-09-27 18:19:43 +0000

    added altimeter driver

---
+++ ivp-extend/trunk/src/iAltimeter_cruzPro/CMakeFiles/iAltimeter_cruzPro.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/iAltimeter_cruzPro.dir/main.cpp.o"
+  "CMakeFiles/iAltimeter_cruzPro.dir/Altimeter_cruzPro.cpp.o"
+  "CMakeFiles/iAltimeter_cruzPro.dir/iAltimeter_cruzPro_nmea.cpp.o"
+  "../../bin/iAltimeter_cruzPro.pdb"
+  "../../bin/iAltimeter_cruzPro"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/iAltimeter_cruzPro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
