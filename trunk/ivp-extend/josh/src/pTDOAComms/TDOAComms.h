f55bbf2274a6b5fe679ee76806283e391aeddef3
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-10-21 21:53:25 +0000

    new tdoa app added protobuf to include directories

---
+++ trunk/ivp-extend/josh/src/pTDOAComms/TDOAComms.h
@@ -0,0 +1,35 @@
+/************************************************************/
+/*    NAME:                                               */
+/*    ORGN: MIT                                             */
+/*    FILE: TDOAComms.h                                          */
+/*    DATE:                                                 */
+/************************************************************/
+
+#ifndef TDOAComms_HEADER
+#define TDOAComms_HEADER
+
+#include "MOOS/libMOOS/MOOSLib.h"
+#include "tdoa.pb.h"
+#include "tdoa_acomms.pb.h"
+
+class TDOAComms : public CMOOSApp
+{
+ public:
+   TDOAComms();
+   ~TDOAComms();
+
+ protected:
+   bool OnNewMail(MOOSMSG_LIST &NewMail);
+   bool Iterate();
+   bool OnConnectToServer();
+   bool OnStartUp();
+   void RegisterVariables();
+
+ private: // Configuration variables
+
+ private: // State variables
+   unsigned int m_iterations;
+   double       m_timewarp;
+};
+
+#endif 
---
+++ trunk/ivp-extend/josh/src/pTDOAComms/TDOAComms_Info.h
@@ -0,0 +1,18 @@
+/****************************************************************/
+/*   NAME:                                              */
+/*   ORGN: MIT Cambridge MA                                     */
+/*   FILE: TDOAComms_Info.h                                      */
+/*   DATE: Dec 29th 1963                                        */
+/****************************************************************/
+
+#ifndef TDOAComms_INFO_HEADER
+#define TDOAComms_INFO_HEADER
+
+void showSynopsis();
+void showHelpAndExit();
+void showExampleConfigAndExit();
+void showInterfaceAndExit();
+void showReleaseInfoAndExit();
+
+#endif
+
