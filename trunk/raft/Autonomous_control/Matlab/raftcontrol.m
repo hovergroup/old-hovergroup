612ebe84ccea3b8e02c3cfbf3420558aa0860622
pvazteixeira@gmail.com <pvazteixeira@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-11-21 19:33:37 +0000

    modified heading controller gains

--- trunk/raft/Autonomous_control/Matlab/raftcontrol.m
+++ trunk/raft/Autonomous_control/Matlab/raftcontrol.m
@@ -1,10 +1,11 @@
-function control = raftcontrol(theta)
+function control = raftcontrol(theta,thetaOld,dt)
 
 %Simple P controller for raft heading, for testing purposes
 thetades=0;
 Kp = 0.01;
+Kd = 0.00;
 thrustmax = pi*Kp;
-thrust = -Kp*(theta-thetades);
+thrust = -Kp*(theta-thetades)-Kd*(theta-thetaOld)/dt;
 
 %now convert thrust to m1d,m1s,m2d,...,m5s
 m1s = 0;
@@ -31,7 +32,7 @@ m5d = 3;
 % m5s = 0;
 % m5d = 3;
 
-control = uint8(['<';'<';'<';m1s; m1d; m2s; m2d; m3s; m3d; m4s; m4d; m5s; m5d;'>';'>';'>']);
+control = uint8(['<';'[';'(';m1s; m1d; m2s; m2d; m3s; m3d; m4s; m4d; m5s; m5d;']';')';'>']);
 %control = uint8(hex2dec(['3C'; '3C'; '3C'; '39'; '03'; '27'; '01'; '19'; '01'; '00'; '03'; '00'; '03'; '3E'; '3E'; '3E']));
 %control = uint8([60 60 60 57 3 39 1 25 1 0 3 0 3 62 62 62]);
 
