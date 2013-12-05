2ef838892976289486862393237669355f52de44
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-12-05 02:28:16 +0000

    fixing scripts, tabs, starting update scripts

--- trunk/ivp-extend/josh/build_proto.sh
+++ trunk/ivp-extend/josh/build_proto.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc --proto_path=src/protobuf --cpp_out=src/protobuf --proto_path=/home/mei/goby/include --proto_path=/usr/include src/protobuf/*.proto
+protoc --proto_path=src/protobuf --cpp_out=src/protobuf --proto_path=/home/josh/goby/include --proto_path=/usr/include src/protobuf/*.proto
