4df16c3b3215a3130b1d8414db7c7335f230ca96
pvazteixeira@gmail.com <pvazteixeira@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-12-06 00:05:29 +0000

    updated position controller

--- trunk/raft/Autonomous_control/Matlab/raftcontrol.m
+++ trunk/raft/Autonomous_control/Matlab/raftcontrol.m
@@ -16,29 +16,29 @@ elseif thrust<=-(255-offset)
 end
 
 %now convert thrust to m1d,m1s,m2d,...,m5s
-m1s = abs(thrust)+offset;
-%m1s = floor(thrust/thrustmax*255);
-m1d = 2-sign(thrust);
-m2s = abs(thrust)+offset;
-m2d = 2+sign(thrust);
-m3s = m2s;
-m3d = 2-sign(thrust);
-m4s = m2s;
-m4d = 2+sign(thrust);
-m5s = 0;
-m5d = 0;
-
-% m1s = 0;
+% m1s = abs(thrust)+offset;
 % %m1s = floor(thrust/thrustmax*255);
-% m1d = 1;
-% m2s = 0;
-% m2d = 1;
-% m3s = 0;
-% m3d = 1;
-% m4s = 0;
-% m4d = 3;
+% m1d = 2-sign(thrust);
+% m2s = abs(thrust)+offset;
+% m2d = 2+sign(thrust);
+% m3s = m2s;
+% m3d = 2-sign(thrust);
+% m4s = m2s;
+% m4d = 2+sign(thrust);
 % m5s = 0;
 % m5d = 0;
+
+m1s = 35;
+%m1s = floor(thrust/thrustmax*255);
+m1d = 1;
+m2s = 0;
+m2d = 1;
+m3s = 0;
+m3d = 1;
+m4s = 0;
+m4d = 3;
+m5s = 0;
+m5d = 0;
 
 control = uint8(['<';'[';'(';m1s; m1d; m2s; m2d; m3s; m3d; m4s; m4d; m5s; m5d;')';']';'>']);
 %control = uint8(hex2dec(['3C'; '3C'; '3C'; '39'; '03'; '27'; '01'; '19'; '01'; '00'; '03'; '00'; '03'; '3E'; '3E'; '3E']));
