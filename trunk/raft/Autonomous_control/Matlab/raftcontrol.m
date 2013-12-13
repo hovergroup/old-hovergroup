93458b4fa58798bad2e6adde98dc546a0bd99ce0
pvazteixeira@gmail.com <pvazteixeira@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-12-13 20:24:01 +0000

    Pedro wrote liveCameraCapture.m Eric updated gains in raftcontrol.asv so m1s = 0.42 m3s

--- trunk/raft/Autonomous_control/Matlab/raftcontrol.m
+++ trunk/raft/Autonomous_control/Matlab/raftcontrol.m
@@ -3,12 +3,13 @@ function control = raftcontrol(theta,thetaOld,dt,thetades, thetaError,IError)
 %Simple PID controller for raft heading, for testing purposes
 
 Kp = 5;
-Kd = 1;
+Kd = 2;
 Ki = 0;
-dt = 0.1;
+% dt = 0.1;
 % thrustmax = pi;
 offset = 30; %to account for motor not moving for commands less than offset
-thrust = Kp*(thetaError) + Kd*(theta - thetaOld)/dt + Ki*IError;
+thrust = -Kp*(thetaError) - Kd*(theta - thetaOld)/dt - Ki*IError;
+
 if thrust>255-offset
     thrust=255-offset;
 elseif thrust<=-(255-offset)
@@ -17,34 +18,25 @@ end
 
 %now convert thrust to m1d,m1s,m2d,...,m5s
 % m1s = abs(thrust)+offset;
-% %m1s = floor(thrust/thrustmax*255);
-% m1d = 2-sign(thrust);
+% m1d = 2+sign(thrust);
 % m2s = abs(thrust)+offset;
-% m2d = 2+sign(thrust);
+% m2d = 2-sign(thrust);
 % m3s = m2s;
-% m3d = 2-sign(thrust);
+% m3d = 2+sign(thrust);
 % m4s = m2s;
-% m4d = 2+sign(thrust);
+% m4d = 2-sign(thrust);
 % m5s = 0;
 % m5d = 0;
 
-m1s = 35;
-%m1s = floor(thrust/thrustmax*255);
+m1s = 53;
 m1d = 1;
 m2s = 0;
 m2d = 1;
-m3s = 0;
-m3d = 1;
+m3s = 125;
+m3d = 3;
 m4s = 0;
 m4d = 3;
 m5s = 0;
 m5d = 0;
 
 control = uint8(['<';'[';'(';m1s; m1d; m2s; m2d; m3s; m3d; m4s; m4d; m5s; m5d;')';']';'>']);
-%control = uint8(hex2dec(['3C'; '3C'; '3C'; '39'; '03'; '27'; '01'; '19'; '01'; '00'; '03'; '00'; '03'; '3E'; '3E'; '3E']));
-%control = uint8([60 60 60 57 3 39 1 25 1 0 3 0 3 62 62 62]);
-
-
-
-
-
--- trunk/raft/Autonomous_control/Matlab/raftcontrolxy.m
+++ trunk/raft/Autonomous_control/Matlab/raftcontrolxy.m
@@ -1,4 +1,4 @@
-function control = raftcontrolxy(theta,thetaOld,thetaDes,thetaError,IError,x,y,xdes1,ydes1)
+function control = raftcontrolxy(theta,thetaOld,thetaDes,thetaError,IError,x,y,xdes1,ydes1,xold1,yold1)
 %compute thrust commands for x, y, and theta and add them together
 %EG
 
@@ -11,50 +11,55 @@ thetades=thetaDes;
 offset = 30; %to account for motor not moving for commands less than offset
 dt=0.1;
 
-Kpx = 0;
-Kpy = 0;
+Kpx = 1;
+Kpy = 5;
 
-xthrust = -Kpx*(x-xdes);
-ythrust = -Kpy*(y-ydes);
+Kdy = 1;
+Kdx = 5;
+
+xthrust = -Kpx*(x-xdes)-Kdx*(x-xold1)/dt;
+ythrust = -Kpy*(y-ydes)-Kdy*(y-yold1)/dt;
 %see pg 77-78 of EG lab notebook for derivation
-M13 = 1/2*(xthrust-ythrust*tan(theta))/(-cos(theta)-sin(theta)*tan(theta))
-M24 = 1/2*(ythrust-M13*sin(theta))/cos(theta)
+M13 = -1/2*(xthrust-ythrust*tan(thetades))/(-cos(theta)-sin(theta)*tan(theta));
+M24 = -1/2*(ythrust-M13*sin(theta))/cos(theta);
 
-M13a = round55(M13,offset);
-M24a = round55(M24,offset);
+M13a = round255(M13,offset);
+M24a = round255(M24,offset);
 %now convert thrust to m1d,m1s,m2d,...,m5s
-m1s = abs(M13a)+offset;
+% m1s = abs(M13a)+offset;
+m1s = 0;
 m1d = 2-sign(M13a);
 m2s = abs(M24a)+offset;
 m2d = 2-sign(M24a);
-m3s = m1sxy;
+% m3s = m1s;
+m3s = 0;
 m3d = 2+sign(M13a);
-m4s = m2sxy;
+m4s = m2s;
 m4d = 2+sign(M24a);
 m5s = 0;
 m5d = 3;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %heading control command calculation
-Kp = 5; %was 5
-Kd = 1; %was 1
+Kp = 0; %was 5
+Kd = 0; %was 1
 Ki = 0;
 dt = 0.1;
 
 thrust = -Kp*(thetaError) - Kd*(theta - thetaOld)/dt - Ki*IError;
 
 %now convert thrust to m1d,m1s,m2d,...,m5s
-m1st = abs(thrust)+offset;
-%m1s = floor(thrust/thrustmax*255);
-m1dt = 2+sign(thrust);
-m2st = abs(thrust)+offset;
-m2dt = 2-sign(thrust);
-m3st = m2st;
-m3dt = 2+sign(thrust);
-m4st = m2st;
-m4dt = 2-sign(thrust);
-m5st = 0;
-m5dt = 0;
+% m1st = abs(thrust)+offset;
+% %m1s = floor(thrust/thrustmax*255);
+% m1dt = 2+sign(thrust);
+% m2st = abs(thrust)+offset;
+% m2dt = 2-sign(thrust);
+% m3st = m2st;
+% m3dt = 2+sign(thrust);
+% m4st = m2st;
+% m4dt = 2-sign(thrust);
+% m5st = 0;
+% m5dt = 0;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %add the thrust commands together
@@ -80,11 +85,13 @@ m5dt = 0;
 % m5s = 0;
 % m5d = 0;
 
-% m1s = abs(round255(M13-thrust,offset))+offset;
+% m1s = abs(round255(0.42*M13-thrust,offset))+offset;
+% % m1s = 0;
 % m1d = 2-sign(M13-thrust);
 % m2s = abs(round255(-M24+thrust,offset))+offset;
 % m2d = 2-sign(-M24+thrust);
 % m3s = abs(round255(-M13-thrust,offset))+offset;
+% % m3s = 0;
 % m3d = 2+sign(M13+thrust);
 % m4s = abs(round255(M24-thrust,offset))+offset;
 % m4d = 2+sign(M24-thrust);
