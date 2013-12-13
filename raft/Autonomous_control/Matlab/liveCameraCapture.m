c1bd86a9b643974ccffe6bd2db93a5b3760a3d6e
pvazteixeira@gmail.com <pvazteixeira@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-12-13 20:24:01 +0000

    Pedro wrote liveCameraCapture.m Eric updated gains in raftcontrol.asv so m1s = 0.42 m3s

--- raft/Autonomous_control/Matlab/liveCameraCapture.m
+++ raft/Autonomous_control/Matlab/liveCameraCapture.m
@@ -9,20 +9,45 @@ close all;
 imaqreset;
 
 % obj = videoinput('winvideo',1,'RGB24_320x240');
-obj = videoinput('winvideo',1,'RGB24_424x240');
-min_area = 50;
-max_area = 200;
+
+% obj = videoinput('winvideo',1,'RGB24_424x240');
+% min_area = 50;
+% max_area = 200;
 
 % obj = videoinput('winvideo',1,'RGB24_640x360');
 
-% obj = videoinput('winvideo',1,'RGB24_800x448');
-% min_area = 400;
-% max_area = 600;
+obj = videoinput('winvideo',1,'RGB24_800x448');
+min_area = 400;
+max_area = 600;
 
 % obj = videoinput('winvideo',1,'RGB24_640x480');
 
 T=1;
 losses = 0;
+
+%set desired position, heading
+xdes = 300;
+ydes = 300;
+thetades = 90; %degrees
+
+%initialize saved variables
+thetaOld = 0;
+xOld = 0;
+yOld = 0;
+IError = 0;
+vecsize = 1000;
+thetasave = zeros(1,vecsize);
+xsave = zeros(1,vecsize);
+ysave = zeros(1,vecsize);
+timevec =zeros(1,vecsize);
+thetaErrorsave = zeros(1,vecsize);
+count = 1;
+
+%open serial port for sending control commands to raft
+s = serial('COM5');
+set(s,'BaudRate', 57600);
+set(s,'ByteOrder','bigEndian');
+fopen(s);
 
 try
     set(obj,'framesperTrigger',5,'TriggerRepeat',Inf);
@@ -78,12 +103,55 @@ try
             % draw in red since we lost packets!
         end
         
+        % get pose from blobs
+           pose = getPoseFromBlobs2(xBlobs);
+           xraft = pose(1);
+           yraft = pose(2);
+           thetaraft = pose(3)*180/pi;
+        
+        % compute errors 
+        thetaError = thetaraft - thetades;
+  
+        if thetaError>180
+            thetaError = thetaError -360;
+        elseif thetaError<-180
+            thetaError = thetaError +360;
+        end
+        thetaErrorsave(1,count) = thetaError;
+        IError = IError + thetaError;
+    
+        if IError > 3
+            IError = 3;
+        end
+        
+        T = toc;
+        if count>1
+            timevec(1,count) = T+timevec(1,count-1);
+        end
+        
+        % run control loop
+        uOut = raftcontrolxy(thetaraft,thetaOld,thetades,thetaError,IError,xraft,yraft,xdes,ydes,xOld,yOld);
+        %uOut = raftcontrol(thetaraft,thetaOld,T,thetades,thetaError,IError);
+        
+        % send commands to raft
+        fwrite(s,uint8(uOut));
+        
+        %make plot
         title(['Frame rate: ', num2str(1.0/T)]);
         xlim([0, width]);
         ylim([0, height]);
         drawnow;
+        
+        %save variables
+        thetaOld = thetaraft;
+        xOld = xraft;
+        yOld = yraft;
+        thetasave(1,count) = thetaraft;
+        xsave(1,count) = xraft;
+        ysave(1,count) = yraft;
+        count = count+1;
 
-        T = toc;
+        
     end
 catch exception
     disp('ERROR:')
