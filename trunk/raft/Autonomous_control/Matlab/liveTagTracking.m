fe19724d8d191dc3f994bdf6ae40fd7883b99420
pvazteixeira@gmail.com <pvazteixeira@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-12-18 19:39:23 +0000

    Improved error handling in getTransform, upped the resolution to get less frame drops.

--- trunk/raft/Autonomous_control/Matlab/liveTagTracking.m
+++ trunk/raft/Autonomous_control/Matlab/liveTagTracking.m
@@ -1,16 +1,22 @@
-clear;
+clear all;
 close all;
+clc;
 
 imaqreset;
 % obj = videoinput('winvideo',2,'MJPG_800x448');  % Pedro's machine
 % obj = videoinput('winvideo',1,'RGB24_640x480');  % Pedro's machine
-% obj = videoinput('winvideo',2,'MJPG_1280x720');  % Pedro's machine
-obj = videoinput('winvideo',2,'M420_960x544');  % Pedro's machine
+obj = videoinput('winvideo',2,'MJPG_1280x720');  % Pedro's machine
+% obj = videoinput('winvideo',2,'M420_960x544');  % Pedro's machine
 % obj = videoinput('winvideo',2,'M420_1280x720');  % Pedro's machine
 
 original = imread('tag.png ');
-
 original = rgb2gray(original);
+
+lastKnownPosition = zeros(2,1);
+
+successes = 0;
+failures = 0;
+
 try
     set(obj,'framesperTrigger',5,'TriggerRepeat',Inf);
     start(obj);
@@ -22,27 +28,24 @@ try
         
         distorted = im(:,:,1);
         
-        try
-            [t,theta,scale] = getTransform(distorted,original);
-            if (0==scale)
-                continue
-            end
-            
-        catch exception
-            hold off;
-            imshow(im(:,:,1));
-            drawnow;
-            disp('ERROR:')
-            disp(exception.message);
-            continue
+        [t,theta,scale] = getTransform(distorted,original);
+        if (0==scale)
+            marker = 'bx';
+            failures = failures + 1;
+        else
+            successes = successes + 1;
+            marker='rx';
+            lastKnownPosition = t;
         end
         
-        hold off;
-        imshow(im(:,:,1));
-        hold on;
-        plot(t(1), t(2),'rx','MarkerSize',15,'LineWidth',3);
+%         hold off;
+%         imshow(im(:,:,1));
+%         hold on;
+        plot(lastKnownPosition(1), lastKnownPosition(2),marker,'MarkerSize',15,'LineWidth',3);
+        xlim([0 1279])
+        ylim([0 719])
         T = toc;
-        title(['Frame rate: ', num2str(1.0/T)]);
+        title(['Frame rate: ', num2str(1.0/T), ' Eric rate: ', num2str(successes/(successes+failures)), ' F: ', num2str(failures)]);
         drawnow;
     end
 catch exception
