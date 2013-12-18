89999093039ef9fd18559aaff8830bc06800a55d
pvazteixeira@gmail.com <pvazteixeira@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-12-18 17:16:22 +0000

    Improved tracking, works with overhead camera at 720p and about 8fps

--- trunk/raft/Autonomous_control/Matlab/getTransform.m
+++ trunk/raft/Autonomous_control/Matlab/getTransform.m
@@ -29,7 +29,7 @@ function [ T, theta, scale ] = getTransform( image, original )
     matchedOriginal  = validPointsOriginal(indexPairs(:,1));
     matchedImage = validPointsImage(indexPairs(:,2));
    
-    if ( 1==size(matchedOriginal,1) || 1 == size(matchedImage,1) )
+    if ( 1>=size(matchedOriginal,1) || 1 >= size(matchedImage,1) )
         return
     end
     
@@ -48,8 +48,12 @@ function [ T, theta, scale ] = getTransform( image, original )
     ss = Tinv(2,1);
     sc = Tinv(1,1);
     scale = sqrt(ss*ss + sc*sc);
-    theta = atan2(ss,sc)*180/pi;
-    T(1) = Tinv(3,1);
-    T(2) = Tinv(3,2);
+    theta = atan2(ss,sc);
+  
+    disp(rad2deg(theta))
+    disp(scale)
+    disp('-------');
+    T(1) = Tinv(3,1) + 60*scale*cos(-theta+pi/4);
+    T(2) = Tinv(3,2) + 60*scale*sin(-theta+pi/4);
 end
 
