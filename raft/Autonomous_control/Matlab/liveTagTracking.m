d56c9a2538a76c3b4519e652c6b4a1bc2f3fe5c3
pvazteixeira@gmail.com <pvazteixeira@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-12-19 02:32:47 +0000

    Final commit for the day:  - minor improvements in liveTagTracking.m  - all plotting has been moved to plotAll.m  - performance seems to be nearly 10fps (with NO plotting) at 1280x720.

--- raft/Autonomous_control/Matlab/liveTagTracking.m
+++ raft/Autonomous_control/Matlab/liveTagTracking.m
@@ -18,7 +18,7 @@ try
     % config:raft
 
     % config:pvt
-    % obj = videoinput('winvideo',2,'MJPG_800x448');
+%     obj = videoinput('winvideo',2,'MJPG_800x448');
     % obj = videoinput('winvideo',1,'RGB24_640x480');
     obj = videoinput('winvideo',2,'MJPG_1280x720');
 %     obj = videoinput('winvideo',2,'M420_960x544');
@@ -62,7 +62,8 @@ successes = 0;  % stores number of frames for which we could track the raft
 failures = 0;   % the ones for which we couldn't
 count = 0;      % how many in total?
 
-preallocate = 1000; % state vector length
+preallocate = 10000; % state vector length
+t = zeros(1, preallocate); 
 x = zeros(3, preallocate); 
 v1 = zeros(3, preallocate); 
 v5 = zeros(3, preallocate); 
@@ -73,32 +74,34 @@ try
 
     % pvt: there's probably a way to write this loop so that it performs
     % faster. Perhaps using a sub-loop that goes through each frame set?
+    tic;
     while islogging(obj);
-        tic;
         count = count + 1;
         im = getdata(obj, 1, 'uint8' );
+        t(count) = toc;
         flushdata(obj);
         
         distorted = rgb2gray(im);
         
-        [t,theta,scale, success] = getTransform(distorted,original);
+        [translation, theta, scale, success] = getTransform(distorted,original);
         
         if (0==success)
             color = 'b';
             failures = failures + 1;
             if(count > 1)
-                x(:, count) = x(:,count-1); % hold
+                x(1:3, count) = x(1:3,count-1); % hold
             end
         else
             successes = successes + 1;
             color='r';
-            x(:, count) = [t; theta];
+            x(1:3, count) = [translation; theta];
         end
         
-        
         if (count > 6)
-            v1(:,count) = x(:,count) - x(:,count-1);
-            v5(:,count) = mean(diff(x(:,count-4:count),1,2),2);
+%             v1(:,count) = x(1:3,count) - x(1:3,count-1);
+%             v1(:,count) = v1(:,count) * (1/(t(count) - t(count-1)));
+            v5(:,count) = mean(diff(x(1:3,count-4:count),1,2),2);
+            v5(:,count) = v5(:,count) * (4/(t(count) - t(count-4)));
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             % BEGIN: RUN CONTROL LOOP HERE %
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -106,72 +109,18 @@ try
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             % END: RUN CONTROL LOOP HERE %
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        end
-         
-        figure(1);
-        % PLOT: POSITION
-        subplot(2,2,1)
-%          hold off;
-%         imshow(im(:,:,1));
-%         hold on;
-        if(count<100)
-            plot(x(1,1:count), x(2,1:count), [color,'.'], 'MarkerSize', 15,'LineWidth',3);
-        else
-            plot(x(1,(count-99):count), x(2,(count-99):count), [color,'.'], 'MarkerSize', 15,'LineWidth',3);
-        end
-        xlabel('x [px]')
-        ylabel('y [px]')
-        xlim([0 1279])
-        ylim([0 719])
-        grid on
-        T = toc;
-        title(['Frame rate: ', num2str(1.0/T), ' S/F: ', num2str(successes), '/', num2str(failures)]);
+                
+            % plotAll(t, x, v5, count, color, successes, failures);    
         
-        % PLOT: HEADING
-        subplot(2,2,2)
-        if(count<100)
-            plot(rad2deg(x(3,1:count)),['-',color,'.'],'LineWidth',3);
-        else
-            plot((count-99):count,rad2deg(x(3,(count-99):count)),['-',color,'.'],'LineWidth',3);
-        end
-        ylabel('Heading [deg]');       
-        grid on
-        
-        if(count > 6)
-            % PLOT: VELOCITY
-            subplot(2,2,3)
-            if(count<100)
-                hold off;
-                plot((v5(1,1:count)),['-',color,'.'],'LineWidth',3);
-                hold on
-                plot((v5(2,1:count)),['--',color,'.'],'LineWidth',3);
-            else
-                                hold off;
-                plot((count-99):count,(v5(1,(count-99):count)),['-',color,'.'],'LineWidth',3);
-                hold on
-                plot((count-99):count,(v5(2,(count-99):count)),['--',color,'.'],'LineWidth',3);
+            disp(count/toc);
+            
+            % resize state vector
+            if size(x,2)-count < 100
+                t = [t, zeros(1, preallocate)];
+                x = [x, zeros(3, preallocate)];
+                v1 = [v1, zeros(3, preallocate)];
+                v5 = [v5, zeros(3, preallocate)];
             end
-            ylabel('Speed [px/s]');       
-            grid on
-
-            % PLOT: YAW RATE
-            subplot(2,2,4)
-            if(count<100)
-                hold off;
-                plot(rad2deg(v5(3,1:count)),['-',color,'.'],'LineWidth',3);
-            else
-                hold off;
-                plot((count-99):count,rad2deg(v5(3,(count-99):count)),['-',color,'.'],'LineWidth',3);
-            end
-            ylabel('Yaw rate [rad/s]');       
-            grid on            
-        end
-        drawnow;
-        
-        % resize state vector
-        if size(x,2)-count < 100
-            x = [x, zeros(3, preallocate)];
-            disp('REALLOC!');
         end
      end
 catch exception
---
+++ raft/Autonomous_control/Matlab/plotAll.m
@@ -0,0 +1,67 @@
+function [ output_args ] = plotAll( t, x, v5, count, color, successes, failures )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+% PLOT: POSITION
+            subplot(2,2,1)
+%             hold off;
+%             imshow(im(:,:,1));
+%             hold on;
+            if(count<100)
+                plot(x(1,1:count), x(2,1:count), [color,'.'], 'MarkerSize', 15,'LineWidth',3);
+            else
+                plot(x(1,(count-99):count), x(2,(count-99):count), [color,'.'], 'MarkerSize', 15,'LineWidth',3);
+            end
+            xlabel('x [px]')
+            ylabel('y [px]')
+            xlim([0 1279])
+            ylim([0 719])
+            grid on
+            %T = toc;
+            title(['Frame rate: ', num2str(5.0/(t(count)-t(count-4))), ' S/F: ', num2str(successes), '/', num2str(failures)]);
+
+            % PLOT: HEADING
+            subplot(2,2,2)
+            if(count<100)
+                plot(t(1:count),rad2deg(x(3,1:count)),['-',color,'.'],'LineWidth',3);
+            else
+                plot(t((count-99):count),rad2deg(x(3,(count-99):count)),['-',color,'.'],'LineWidth',3);
+            end
+            xlabel('Time [s]')
+            ylabel('Heading [deg]');       
+            grid on
+        
+
+            % PLOT: VELOCITY
+            subplot(2,2,3)
+            if(count<100)
+                hold off;
+                plot(t(1:count),(v5(1,1:count)),['-',color,'.'],'LineWidth',3);
+                hold on
+                plot(t(1:count),(v5(2,1:count)),['--',color,'.'],'LineWidth',3);
+            else
+                hold off;
+                plot(t((count-99):count),(v5(1,(count-99):count)),['-',color,'.'],'LineWidth',3);
+                hold on
+                plot(t((count-99):count),(v5(2,(count-99):count)),['--',color,'.'],'LineWidth',3);
+            end
+            xlabel('Time [s]')
+            ylabel('Speed [px/s]');       
+            grid on
+
+            % PLOT: YAW RATE
+            subplot(2,2,4)
+            if(count<100)
+                hold off;
+                plot(t(1:count),rad2deg(v5(3,1:count)),['-',color,'.'],'LineWidth',3);
+            else
+                hold off;
+                plot(t((count-99):count),rad2deg(v5(3,(count-99):count)),['-',color,'.'],'LineWidth',3);
+            end
+            xlabel('Time [s]')
+            ylabel('Yaw rate [deg/s]');       
+            grid on            
+
+        drawnow;
+end
+
