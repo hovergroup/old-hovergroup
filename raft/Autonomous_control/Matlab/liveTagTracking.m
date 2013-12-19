1eb83cbcdc852892a2839aeb525743bbac78f9d9
pvazteixeira@gmail.com <pvazteixeira@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-12-19 01:56:01 +0000

    liveTagTracking.m now performs even better (except for framerate)

--- raft/Autonomous_control/Matlab/liveTagTracking.m
+++ raft/Autonomous_control/Matlab/liveTagTracking.m
@@ -34,7 +34,7 @@ try
     src.Exposure = -7;      % -13 to 0 (low to high)
     src.FocusMode='manual';
     src.Focus = 0;          % seems to work ok with our FOV
-    src.FrameRate = '15.0000';
+    src.FrameRate = '30.0000';
     pause(1);
 catch exception
     disp('ERROR:')
@@ -44,7 +44,14 @@ catch exception
     return
 end
 
-%% Maker
+%% Raft communication
+
+% s = serial('COM5');
+% set(s,'BaudRate', 57600);
+% set(s,'ByteOrder','bigEndian');
+% fopen(s);
+
+%% Marker
 
 original = imread('tag.png ');
 original = rgb2gray(original);
@@ -57,6 +64,8 @@ count = 0;      % how many in total?
 
 preallocate = 1000; % state vector length
 x = zeros(3, preallocate); 
+v1 = zeros(3, preallocate); 
+v5 = zeros(3, preallocate); 
 
 %% Main loop
 try
@@ -68,8 +77,8 @@ try
         tic;
         count = count + 1;
         im = getdata(obj, 1, 'uint8' );
-            flushdata(obj);
-% 
+        flushdata(obj);
+        
         distorted = rgb2gray(im);
         
         [t,theta,scale, success] = getTransform(distorted,original);
@@ -85,14 +94,26 @@ try
             color='r';
             x(:, count) = [t; theta];
         end
-           
+        
+        
+        if (count > 6)
+            v1(:,count) = x(:,count) - x(:,count-1);
+            v5(:,count) = mean(diff(x(:,count-4:count),1,2),2);
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % BEGIN: RUN CONTROL LOOP HERE %
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % END: RUN CONTROL LOOP HERE %
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        end
+         
         figure(1);
-        subplot(1,2,1)
-%                 hold off;
+        % PLOT: POSITION
+        subplot(2,2,1)
+%          hold off;
 %         imshow(im(:,:,1));
 %         hold on;
-
         if(count<100)
             plot(x(1,1:count), x(2,1:count), [color,'.'], 'MarkerSize', 15,'LineWidth',3);
         else
@@ -105,7 +126,9 @@ try
         grid on
         T = toc;
         title(['Frame rate: ', num2str(1.0/T), ' S/F: ', num2str(successes), '/', num2str(failures)]);
-        subplot(1,2,2)
+        
+        % PLOT: HEADING
+        subplot(2,2,2)
         if(count<100)
             plot(rad2deg(x(3,1:count)),['-',color,'.'],'LineWidth',3);
         else
@@ -113,8 +136,39 @@ try
         end
         ylabel('Heading [deg]');       
         grid on
+        
+        if(count > 6)
+            % PLOT: VELOCITY
+            subplot(2,2,3)
+            if(count<100)
+                hold off;
+                plot((v5(1,1:count)),['-',color,'.'],'LineWidth',3);
+                hold on
+                plot((v5(2,1:count)),['--',color,'.'],'LineWidth',3);
+            else
+                                hold off;
+                plot((count-99):count,(v5(1,(count-99):count)),['-',color,'.'],'LineWidth',3);
+                hold on
+                plot((count-99):count,(v5(2,(count-99):count)),['--',color,'.'],'LineWidth',3);
+            end
+            ylabel('Speed [px/s]');       
+            grid on
+
+            % PLOT: YAW RATE
+            subplot(2,2,4)
+            if(count<100)
+                hold off;
+                plot(rad2deg(v5(3,1:count)),['-',color,'.'],'LineWidth',3);
+            else
+                hold off;
+                plot((count-99):count,rad2deg(v5(3,(count-99):count)),['-',color,'.'],'LineWidth',3);
+            end
+            ylabel('Yaw rate [rad/s]');       
+            grid on            
+        end
         drawnow;
         
+        % resize state vector
         if size(x,2)-count < 100
             x = [x, zeros(3, preallocate)];
             disp('REALLOC!');
@@ -123,6 +177,10 @@ try
 catch exception
     disp('ERROR:')
     disp(exception.message);
+    disp('FILE:')
+    disp(exception.stack.file);
+    disp('LINE:')
+    disp(exception.stack.line);
     stop(obj);
     imaqreset
 end
