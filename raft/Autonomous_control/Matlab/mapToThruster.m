0553f7c0818f6867e54577db6d9650ace709e4da
pvazteixeira@gmail.com <pvazteixeira@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2013-12-20 16:46:09 +0000

    Added function to map thrust in the global frame to the body frame.

--- raft/Autonomous_control/Matlab/mapToThruster.m
+++ raft/Autonomous_control/Matlab/mapToThruster.m
@@ -1,16 +1,37 @@
-function [thrust, direction] = mapToThruster(controlInput)
+function [thrust, direction] = mapToThruster(thrustX, thrustY, omega, currentHeading)
 %mapToThruster map control signals to individual raft thrusters
-%   The purpose of this function is to hide the implementation
-%		details from the actual controller
+%   The purpose of this function is to hide the implementation details from
+%   the actual controller. It should take the desired thrust in X and Y,
+%   and the desired angular velocity, and map it to the correct thrusters.
 %
 %   Pedro Vaz Teixeira
 
-	thrust = zeros(5,1);
-	direction = zeros(5,1);
 
-	orientation = [1 -1 1 -1 1];
-	deadband = [35 35 35 35 25];
-	for i=1:5;	
-		thrust = abs(controlInput(i)) * (255 - deadband(i)) + deadband(i);
-		direction = 2 + sign(controlInput(i));
-	end
+%{
+X IS FORWARD
+Y IS STARBOARD
+Z IS UP
+Thrusters:
+#   POSITION    ORIENTATION     ROTATION
+1   FORWARD     +Y              >0 (CCW)
+2   STARBOARD   -X              <0 (CW)
+3   AFT         -Y              >0 (CCW)
+4   PORT        +X              <0 (CW)
+%}
+	thrust = zeros(4,1);
+    direction = zeros(4,1);
+    
+    thrust(1) = thrustX*sin(currentHeading) + thrustY*cos(currentHeading) + omega;
+    thrust(2) = -1* (thrustX*cos(currentHeading) + thrustY*sin(currentHeading) + omega);
+    thrust(3) = -1*(thrustX*sin(currentHeading) + thrustY*cos(currentHeading) + omega);
+    thrust(4) = thrustX*cos(currentHeading) + thrustY*sin(currentHeading) + omega;
+    
+    for i=1:4
+        if thrust(i)>0
+            thrust(i) = min(thrust(i),255);
+            direction(i) = 3;
+        else
+            thrust(i) = max(thrust(i),-255);
+            direction(i) = 1;
+        end 
+    end
