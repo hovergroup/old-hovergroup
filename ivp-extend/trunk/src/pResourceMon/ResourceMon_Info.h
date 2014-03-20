bf17286bcfa4fee94b2af5022c5dd0fb199efd9b
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2014-03-20 21:56:51 +0000

    added pResourceMon and moved system time posting out of acomms driver

---
+++ ivp-extend/trunk/src/pResourceMon/ResourceMon_Info.h
@@ -0,0 +1,18 @@
+/****************************************************************/
+/*   NAME:                                              */
+/*   ORGN: MIT Cambridge MA                                     */
+/*   FILE: ResourceMon_Info.h                                      */
+/*   DATE: Dec 29th 1963                                        */
+/****************************************************************/
+
+#ifndef ResourceMon_INFO_HEADER
+#define ResourceMon_INFO_HEADER
+
+void showSynopsis();
+void showHelpAndExit();
+void showExampleConfigAndExit();
+void showInterfaceAndExit();
+void showReleaseInfoAndExit();
+
+#endif
+
