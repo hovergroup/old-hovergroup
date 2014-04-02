de3cb481aaf27228d324e8855204c110d0e2bd87
pvazteixeira@gmail.com <pvazteixeira@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2014-04-02 21:50:27 +0000

    new raft model fits, new LQR, kalman parameters in controller, 3dt delay betweeen control signal and thrust in raft model

--- trunk/raft/Autonomous_control/Matlab/HeadingFitTF.m
+++ trunk/raft/Autonomous_control/Matlab/HeadingFitTF.m
@@ -3,87 +3,156 @@
  close all
 %Load Data from Tests
 count = 100;
-%--------
+start = 20;
+tend = 35;
+%%
 %load('C:\Users\bubba\Dropbox\data\HeadingTestThrust250.mat')
 %load('C:\Users\bubba\Dropbox\data\HeadingTestDeadbandThrust255.mat')
 load('C:\Users\bubba\Dropbox\data\HeadingTestMarch26_255a.mat')
-theta0 = rad2deg(x(3,20));
-headingvec = rad2deg(x(3,1:count))-theta0;
+theta0 = rad2deg(x(3,start));
+headingvec250 = rad2deg(x(3,1:count))-theta0;
  window = [sin(pi/6) sin(pi/3), sin(pi/2) sin(pi/3) sin(pi/6)];
- c = conv(headingvec,window,'same')/sum(window); 
+c = conv(headingvec250,window,'same')/sum(window); 
+ %c=headingvec250;
 headingratevec250 = diff(c)./diff(t(1:size(c,2)));
-tvec250 = t(20:60)-t(20);
-%--------
+tvec250 = t(start:tend)-t(start);
+%%
 %load('C:\Users\bubba\Dropbox\data\HeadingTestThrust200.mat')
 %load('C:\Users\bubba\Dropbox\data\HeadingTestDeadbandThrust200c.mat')
 load('C:\Users\bubba\Dropbox\data\HeadingTestMarch26_200a.mat')
-theta0 = rad2deg(x(3,20));
-headingvec = rad2deg(x(3,1:count))-theta0;
+theta0 = rad2deg(x(3,start));
+headingvec200 = rad2deg(x(3,1:count))-theta0;
  window = [sin(pi/6) sin(pi/3), sin(pi/2) sin(pi/3) sin(pi/6)];
- c = conv(headingvec,window,'same')/sum(window); 
+c = conv(headingvec200,window,'same')/sum(window); 
+ %c=headingvec200;
 headingratevec200 = diff(c)./diff(t(1:size(c,2)));
-tvec200 = t(20:60)-t(20);
-%--------
+tvec200 = t(start:tend)-t(start);
+%%
 %load('C:\Users\bubba\Dropbox\data\HeadingTestThrust150.mat')
 %load('C:\Users\bubba\Dropbox\data\HeadingTestDeadbandThrust150c.mat')
 load('C:\Users\bubba\Dropbox\data\HeadingTestMarch26_150a.mat')
-theta0 = rad2deg(x(3,20));
-headingvec = rad2deg(x(3,1:count))-theta0;
+theta0 = rad2deg(x(3,start));
+headingvec150 = rad2deg(x(3,1:count))-theta0;
  window = [sin(pi/6) sin(pi/3), sin(pi/2) sin(pi/3) sin(pi/6)];
- c = conv(headingvec,window,'same')/sum(window); 
-headingratevec150 = diff(c)./diff(t(1:size(c,2)));
-tvec150 = t(20:60)-t(20);
-%--------
+c = conv(headingvec150,window,'same')/sum(window); 
+% c=headingvec150;
+ headingratevec150 = diff(c)./diff(t(1:size(c,2)));
+tvec150 = t(start:tend)-t(start);
+%%
 %load('C:\Users\bubba\Dropbox\data\HeadingTestThrust100.mat')
 %load('C:\Users\bubba\Dropbox\data\HeadingTestDeadbandThrust100.mat')
 load('C:\Users\bubba\Dropbox\data\HeadingTestMarch26_100a.mat')
-theta0 = rad2deg(x(3,20));
-headingvec = rad2deg(x(3,1:count))-theta0;
+theta0 = rad2deg(x(3,start));
+headingvec100 = rad2deg(x(3,1:count))-theta0;
  window = [sin(pi/6) sin(pi/3), sin(pi/2) sin(pi/3) sin(pi/6)];
- c = conv(headingvec,window,'same')/sum(window); 
+c = conv(headingvec100,window,'same')/sum(window); 
+%c=headingvec100;
 headingratevec100 = diff(c)./diff(t(1:size(c,2)));
-tvec100 = t(20:60)-t(20);
-%--------
+tvec100 = t(start:tend)-t(start);
+%%
 load('C:\Users\bubba\Dropbox\data\HeadingTestMarch26_50a.mat')
-theta0 = rad2deg(x(3,20));
-headingvec = rad2deg(x(3,1:count))-theta0;
+theta0 = rad2deg(x(3,start));
+headingvec50 = rad2deg(x(3,1:count))-theta0;
  window = [sin(pi/6) sin(pi/3), sin(pi/2) sin(pi/3) sin(pi/6)];
- c = conv(headingvec,window,'same')/sum(window); 
-headingratevec50 = diff(c)./diff(t(1:size(c,2)));
-tvec50 = t(20:60)-t(20);
-%--------
-
+c = conv(headingvec50,window,'same')/sum(window); 
+%c=headingvec50;
+ headingratevec50 = diff(c)./diff(t(1:size(c,2)));
+tvec50 = t(start:tend)-t(start);
+%%
+dt=0.105;
 %Calculate Model
-s=tf([1 0],1);
+%use fminsearch to find optimal coefficients
+ weights = [1 1 4000 4000 5000];
+% costfun = @(z)sum(weights(1)*(Stepmags2(z,start,tend)-headingratevec250(start:tend)).^2+weights(2)*(230/255*Stepmags2(z,start,tend)-headingratevec200(start:tend)).^2+weights(3)*(180/255*Stepmags2(z,start,tend)-headingratevec150(start:tend)).^2+weights(4)*(130/255*Stepmags2(z,start,tend)-headingratevec100(start:tend)).^2+weights(5)*(80/255*Stepmags2(z,start,tend)-headingratevec50(start:tend)).^2);
+% costfun = @(z)sum(weights(1)*(Stepmags2h(z,start,tend)-headingvec250(start:tend)).^2+weights(2)*(230/255*Stepmags2h(z,start,tend)-headingvec200(start:tend)).^2+weights(3)*(180/255*Stepmags2h(z,start,tend)-headingvec150(start:tend)).^2+weights(4)*(130/255*Stepmags2h(z,start,tend)-headingvec100(start:tend)).^2+weights(5)*(80/255*Stepmags2h(z,start,tend)-headingvec50(start:tend)).^2); 
+% xopt = fminsearch(costfun,[1.4, 4, 58*1.4*4]);
+% a = xopt(1);
+% b = xopt(2);
+% K = xopt(3);
 %second-order system
 % a=1.4;
-% b=4;
-% K = 54*a*b;
+% b=5;
+% K = 58*a*b;
+
+% s=tf([1 0],1);
 % sys = K/((s+a)*(s+b));
+% syshead = sys/s;
 % [ys,ts] = step(sys,4);
+% [ysh,tsh] = step(syshead,4);
 
 %first order system with delay
-a=1.4;
-K=54*a;
-T = 0.24;
-dt = 0.12;
+% dt=0.12;
+% costfun = @(z)sum(weights(1)*(Stepmags1h(z,start,tend)-headingvec250(start:tend)).^2+weights(2)*(230/255*Stepmags1h(z,start,tend)-headingvec200(start:tend)).^2+weights(3)*(180/255*Stepmags1h(z,start,tend)-headingvec150(start:tend)).^2+weights(4)*(130/255*Stepmags1h(z,start,tend)-headingvec100(start:tend)).^2+weights(5)*(80/255*Stepmags1h(z,start,tend)-headingvec50(start:tend)).^2); 
+% xopt = fminsearch(costfun,[1.4, 80]);
+a = 2.0905;
+K = 99.7466;
+% a=xopt(1);
+% K=xopt(2);
+T = 3*dt;
+ s=tf([1 0],1);
 sys = K/(s+a)*exp(-s*T);
-sysd = c2d(sys,dt);
-[ys,ts] = step(sysd,4);
+syshead = sys/s;
+[ys,ts] = step(sys,4);
+[ysh,tsh] = step(syshead,4);
 
 figure
-plot(tvec250,headingratevec250(20:60),'--k.',ts,ys,'-r',tvec200,headingratevec200(20:60),'--g.',tvec150,headingratevec150(20:60)+1,'--b.',tvec100,headingratevec100(20:60)+1,'--m.',tvec50,headingratevec50(20:60)+1,'--c.','LineWidth',3);
+plot(tvec250,headingratevec250(start:tend),'--k.',ts,ys,'-r',tvec200,headingratevec200(start:tend),'--g.',tvec150,headingratevec150(start:tend),'--b.',tvec100,headingratevec100(start:tend),'--m.',tvec50,headingratevec50(start:tend),'--c.','LineWidth',3);
 xlabel('Time [s]','FontSize',16)
 ylabel('Heading Rate [deg/sec]','FontSize',16);
 set(gca,'FontSize',16)
-axis([0 4 0 60]);
+axis([0 dt*(tend-start) 0 40]);
 h0=legend('Heading Rate Data','Model');
-rect = [0.65, 0.27, 0.05, 0.05];
+rect = [0.65, 0.17, 0.05, 0.05];
 set(h0,'Position',rect);
 grid on
 hold on
 plot(ts,ys*230/255,'-r',ts,ys*180/255,'-r',ts,ys*130/255,'-r',ts,ys*80/255,'-r','LineWidth',3)
 
-print(gcf, '-depsc','RaftModel4')
-print(gcf, '-dpng','RaftModel4')
-print(gcf, '-djpeg','RaftModel4')
+% print(gcf, '-depsc','RaftModel7')
+% print(gcf, '-dpng','RaftModel7')
+% print(gcf, '-djpeg','RaftModel7')
+
+figure
+plot(tvec250,headingvec250(start:tend),'--k.',tsh,ysh,'-r',tvec200,headingvec200(start:tend),'--g.',tvec150,headingvec150(start:tend),'--b.',tvec100,headingvec100(start:tend),'--m.',tvec50,headingvec50(start:tend),'--c.','LineWidth',3);
+xlabel('Time [s]','FontSize',16)
+ylabel('Heading [deg]','FontSize',16);
+set(gca,'FontSize',16)
+axis([0 dt*(tend-start) 0 20]);
+h0=legend('Heading Rate Data','Model');
+rect = [0.65, 0.17, 0.05, 0.05];
+set(h0,'Position',rect);
+grid on
+hold on
+plot(tsh,ysh*230/255,'-r',tsh,ysh*180/255,'-r',tsh,ysh*130/255,'-r',tsh,ysh*80/255,'-r','LineWidth',3)
+
+% 
+% K=220;
+% a=2;
+% 
+% sysh100 = K/(s^2+a*s)*8/(3*pi)*1/(s+8/(3*pi)*1);
+% [y100,t100] = step(sysh100,4);
+% sysh90 = K/(s^2+a*s)*8/(3*pi)*0.9^2/(s+8/(3*pi)*0.9^2);
+% [y90,t90] = step(sysh90,4);
+% sysh70 = K/(s^2+a*s)*8/(3*pi)*0.7^2/(s+8/(3*pi)*0.7^2);
+% [y70,t70] = step(sysh70,4);
+% sysh50 = K/(s^2+a*s)*8/(3*pi)*0.5^2/(s+8/(3*pi)*0.5^2);
+% [y50,t50] = step(sysh50,4);
+% sysh30 = K/(s^2+a*s)*8/(3*pi)*0.3^2/(s+8/(3*pi)*0.3^2);
+% [y30,t30] = step(sysh30,4);
+% 
+% figure
+% plot(tvec250,headingvec250(start:tend),'--k.',t100,y100,'-r',tvec200,headingvec200(start:tend),'--g.',tvec150,headingvec150(start:tend),'--b.',tvec100,headingvec100(start:tend),'--m.',tvec50,headingvec50(start:tend),'--c.','LineWidth',3);
+% xlabel('Time [s]','FontSize',16)
+% ylabel('Heading [deg]','FontSize',16);
+% set(gca,'FontSize',16)
+% axis([0 dt*(tend-start) 0 20]);
+% h0=legend('Heading Rate Data','Model');
+% rect = [0.65, 0.17, 0.05, 0.05];
+% set(h0,'Position',rect);
+% grid on
+% hold on
+% plot(t90,y90,'-r',t70,y70,'-r',t50,y50,'-r',t30,y30,'-r','LineWidth',3)
+
+
+
+
--- trunk/raft/Autonomous_control/Matlab/liveTagTracking.m
+++ trunk/raft/Autonomous_control/Matlab/liveTagTracking.m
@@ -104,8 +104,8 @@ utheta = 0;
 uthetasave = zeros(1,preallocate);
 % xest = [0 0 0]';
 % xestsave = zeros(3,preallocate);
-xest = [0 0]';
-xestsave = zeros(2,preallocate);
+xest = zeros(5,1);
+xestsave = zeros(size(xest,1),preallocate);
 
 lossvec = zeros(1,preallocate);
 gsim = zeros(3,1);
@@ -191,7 +191,7 @@ currentGoal = 1;
             end
             
             %add delay to measurement - ewg march 10 2014
-            delay = 5; %delay, in timesteps, in sensor channel
+            delay = 3; %delay, in timesteps, in sensor channel
             if count>delay
                xdelay = x(:,count-delay); 
             else
@@ -205,22 +205,22 @@ currentGoal = 1;
             IError = x(:,count) - xDesired(:,currentGoal);
             %thrustvec = headingControl(0,x(1,count),0.1);
             %thrustvec = raftcontrolxy3(x(:,count),v5(:,count),xDesired(:,currentGoal),IError);
-            thrustvec = ConstThrust(count);
+            %thrustvec = ConstThrust(count);
             %[control1, control2, control3, xest, utheta, loss, gsim, psisim,Yk1k1,Ykk1] = HeadingControlMIF(x(:,count),xest,utheta,gsim,psisim,Yk1k1,Ykk1);
             %[control1, control2, control3, xest, utheta, loss, gsim, psisim] = HeadingControlMIFconstY(x(:,count),xest,utheta,gsim,psisim);
             
-            %[control1, control2, control3, xest, utheta, loss] = HeadingControlLQG2(xdelay,xest,utheta,PacketLoss);
-            %uthetasave(count) = utheta;
+            [control1, control2, control3, xest, utheta, loss] = HeadingControlLQG2(xdelay,xest,utheta,PacketLoss);
+            uthetasave(count) = utheta;
             
-            %xestsave(:,count) = xest;
+            xestsave(:,count) = xest;
             %---LZ, LH
             %[control1, control2, control3, yout, uout, loss] = HeadingControlLZLH(xdelay,yout,uout,PacketLoss);
             %xest(1,count) = yout(1);
             %utheta = uout(1);
             %uthetasave(count) = utheta;
             %---
-            %lossvec(count) = loss;
-            %thrustvec = [control1 control2 control3];
+            lossvec(count) = loss;
+            thrustvec = [control1 control2 control3];
             
             
             [thrust, direction] = mapToThruster(thrustvec(1), thrustvec(2), thrustvec(3), x(3,count));
