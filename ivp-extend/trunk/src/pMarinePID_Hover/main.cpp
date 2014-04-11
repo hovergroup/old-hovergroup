fd3b1ddb5e49436fe687f77c2268e1b2134d444d
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2014-04-11 00:05:35 +0000

    fixed genmoosapp scripts and existing apps to use their actual names, not arg[0]

--- ivp-extend/trunk/src/pMarinePID_Hover/main.cpp
+++ ivp-extend/trunk/src/pMarinePID_Hover/main.cpp
@@ -31,7 +31,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
   string mission_file;
-  string run_command = argv[0];
+  string run_command = "pMarinePID_Hover";
 
   for(int i=1; i<argc; i++) {
     string argi = argv[i];
