8f3eeaf0b9c28f4b8a796c711434706e1a01a4f6
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2014-08-20 20:55:55 +0000

    working on speed controller based on a linear thrust->speed fit

--- ivp-extend/trunk/src/pMarinePID_Hover/MarinePID.h
+++ ivp-extend/trunk/src/pMarinePID_Hover/MarinePID.h
@@ -34,65 +34,68 @@
 #include "MOOS/libMOOS/MOOSLib.h"
 #include "PIDEngine.h"
 
-class MarinePID : public CMOOSApp
-{
+class MarinePID: public CMOOSApp {
 public:
-  MarinePID();
-  virtual ~MarinePID() {};
-  
-  bool OnNewMail(MOOSMSG_LIST &NewMail);
-  bool Iterate();
-  bool OnConnectToServer();
-  bool OnStartUp();
-  void postCharStatus();
-  void postAllStop();
-  void registerVariables();
-  bool handleYawSettings();
-  bool handleSpeedSettings();
-  bool handleDepthSettings();
+    MarinePID();
+    virtual ~MarinePID() {
+    }
+    ;
+
+    bool OnNewMail(MOOSMSG_LIST &NewMail);
+    bool Iterate();
+    bool OnConnectToServer();
+    bool OnStartUp();
+    void postCharStatus();
+    void postAllStop();
+    void registerVariables();
+    bool handleYawSettings();
+    bool handleSpeedSettings();
+    bool handleDepthSettings();
 
 protected:
-  bool     m_has_control;
-  bool     m_allow_overide;
-  bool     m_allstop_posted;
-  double   m_speed_factor;
+    bool m_has_control;
+    bool m_allow_overide;
+    bool m_allstop_posted;
+    double m_speed_factor;
 
-  double   m_current_heading;
-  double   m_current_speed;
-  double   m_current_depth;
-  double   m_current_pitch;
+    double m_current_heading;
+    double m_current_speed;
+    double m_current_depth;
+    double m_current_pitch;
 
-  double   m_rudder_bias_duration;
-  double   m_rudder_bias_limit;
-  double   m_rudder_bias_side;
-  double   m_rudder_bias_timestamp;
+    double m_rudder_bias_duration;
+    double m_rudder_bias_limit;
+    double m_rudder_bias_side;
+    double m_rudder_bias_timestamp;
 
-  double   m_desired_heading;
-  double   m_desired_speed;
-  double   m_desired_depth;
-  double   m_current_thrust;
+    double m_desired_heading;
+    double m_desired_speed;
+    double m_desired_depth;
+    double m_current_thrust;
 
-  double   m_max_pitch;
-  double   m_max_rudder;
-  double   m_max_thrust;
-  double   m_max_elevator;
+    double m_max_pitch;
+    double m_max_rudder;
+    double m_max_thrust;
+    double m_max_elevator;
 
 public:
-  PIDEngine    m_pengine;
+    PIDEngine m_pengine;
 
 protected:
-  std::string  m_verbose;
+    std::string m_verbose;
 
-  int     m_iteration;
-  double  m_start_time;
-  bool    m_depth_control;
-  bool    m_paused;
+    int m_iteration;
+    double m_start_time;
+    bool m_depth_control;
+    bool m_paused;
 
-  double  m_time_of_last_helm_msg;
-  double  m_time_of_last_nav_msg;
+    double m_time_of_last_helm_msg;
+    double m_time_of_last_nav_msg;
 
-  double  m_tardy_helm_thresh;
-  double  m_tardy_nav_thresh;
+    double m_tardy_helm_thresh;
+    double m_tardy_nav_thresh;
+
+    SpeedControlType m_speed_control_type;
 };
 #endif 
 
