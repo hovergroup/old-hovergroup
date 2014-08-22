6dd46ed50f34a67bf3dd96db2c84b01361161cab
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2014-08-22 18:28:06 +0000

    testing different stuff with dccl updated gains for pid

--- ivp-extend/trunk/src/protobufExample/protobufDCCLExample.cpp
+++ ivp-extend/trunk/src/protobufExample/protobufDCCLExample.cpp
@@ -10,22 +10,31 @@
 
 #include "goby/acomms/dccl.h"
 
+using namespace std;
+
+
 int main() {
 	double nav_x = 320.5423;
 	double nav_y = 50.34;
 
-	VehicleStatus vs;
+//	VehicleStatus vs;
 	VehicleStatusDCCL vsd;
+	for (int i=0; i<30; i++) {
+	    vsd.add_vel_command(i);
+	}
 
-	vs.set_nav_x(nav_x);
-	vs.set_nav_y(nav_y);
-	vs.set_health(VehicleStatus_HealthEnum_HEALTH_GOOD);
+//
+//	vs.set_nav_x(nav_x);
+//	vs.set_nav_y(nav_y);
+//	vs.set_health(VehicleStatus_HealthEnum_HEALTH_GOOD);
+//
+//	vsd.set_nav_x(nav_x);
+//	vsd.set_nav_y(nav_y);
+//	vsd.set_health(VehicleStatusDCCL_HealthEnum_HEALTH_GOOD);
 
-	vsd.set_nav_x(nav_x);
-	vsd.set_nav_y(nav_y);
-	vsd.set_health(VehicleStatusDCCL_HealthEnum_HEALTH_GOOD);
+	cout << vsd.DebugString() << endl;
 
-	std::cout << "Without dccl: " << vs.SerializeAsString().size() << std::endl;
+	std::cout << "Without dccl: " << vsd.SerializeAsString().size() << std::endl;
 	goby::acomms::DCCLCodec* codec = goby::acomms::DCCLCodec::get();
 	try {
 		codec->validate<VehicleStatusDCCL>();
