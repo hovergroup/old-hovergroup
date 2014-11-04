ad2f298708a31703a626ab508ec7ceb112a28554
leighton.josh@gmail.com <leighton.josh@gmail.com@0647bbc8-00a5-446d-4af4-27b70130bfba>
2014-11-04 06:51:32 +0000

    hopefully working version of roboteq driver

--- ivp-extend/raft/src/iRaftRoboteq/RaftRoboteq.cpp
+++ ivp-extend/raft/src/iRaftRoboteq/RaftRoboteq.cpp
@@ -20,7 +20,18 @@ using namespace boost::asio;
 RaftRoboteq::RaftRoboteq() :
                 sock(io_service),
                 deadline_timer(io_service) {
-    tcpReadBuffer = vector<char>(1000, 0);
+    slow_queries.push_back("?V\n");
+    slow_queries.push_back("?T\n");
+    slow_queries.push_back("?A\n");
+    slow_queries.push_back("?BA\n");
+    slow_queries.push_back("?DI 1\n");
+    slow_query_index = 0;
+
+    power_report_count = 0;
+    power_command_count = 0;
+    power_ack_count = 0;
+    power_nack_count = 0;
+    last_report_time = -1;
 }
 
 //---------------------------------------------------------
@@ -37,6 +48,18 @@ bool RaftRoboteq::OnNewMail(MOOSMSG_LIST &NewMail) {
 
     for (p = NewMail.begin(); p != NewMail.end(); p++) {
         CMOOSMsg &msg = *p;
+        string key = msg.GetKey();
+        if (key == "DESIRED_THRUST_LEFT") {
+            setThrust(2, msg.GetDouble());
+        } else if (key == "DESIRED_THRUST_RIGHT") {
+            setThrust(1, msg.GetDouble());
+        } else if (key == "ECA_ARM_POWER", 0) {
+            if (msg.GetDouble() == 0) {
+                setArmPower(false);
+            } else {
+                setArmPower(true);
+            }
+        }
     }
 
     return (true);
@@ -46,6 +69,9 @@ bool RaftRoboteq::OnNewMail(MOOSMSG_LIST &NewMail) {
 // Procedure: OnConnectToServer
 
 bool RaftRoboteq::OnConnectToServer() {
+    m_Comms.Register("DESIRED_THRUST_LEFT", 0);
+    m_Comms.Register("DESIRED_THRUST_RIGHT", 0);
+    m_Comms.Register("ECA_ARM_POWER", 0);
 
     return (true);
 }
@@ -55,30 +81,6 @@ bool RaftRoboteq::OnConnectToServer() {
 //            happens AppTick times per second
 
 bool RaftRoboteq::Iterate() {
-//    boost::system::error_code error;
-//    boost::asio::streambuf response;
-//    boost::asio::read_until(sock, response, "\n");
-//    //int n = sock.read_some(boost::asio::buffer(tcpReadBuffer, 100));
-//    //if (n > 0) {
-//    std::istream response_stream(&response);
-//    cout << response_stream.rdbuf();
-        //cout << string(tcpReadBuffer.begin(), tcpReadBuffer.begin() += n) << endl;
-    //}
-
-//    ssize_t n = recv(m_tcp_sockfd, (void*)&tcpReadBuffer[0], 1000, 0);
-//    //int n = read(m_tcp_sockfd, &tcpReadBuffer[0], 1000);
-//    //if (n < 0) {
-//    //    std::cout << "error reading from socket" << std::endl;
-//    //    return false;
-//    //}
-//    if (n > 0) {
-//        string_buffer += std::string(tcpReadBuffer.begin(),
-//                tcpReadBuffer.begin() += n);
-//
-//        cout << string_buffer;
-//        string_buffer = "";
-//    }
-//    cout << endl;
 
     return (true);
 }
@@ -112,6 +114,22 @@ void RaftRoboteq::io_loop() {
     cout << "Finished running io service" << endl;
 }
 
+
+void RaftRoboteq::handle_eca_power_write(const boost::system::error_code& ec) {
+    if (!ec) {
+    } else {
+        cout << "Error on eca power write: " << ec.message() << endl;
+    }
+}
+
+void RaftRoboteq::handle_command_write(const boost::system::error_code& ec) {
+    if (!ec) {
+        power_command_count++;
+    } else {
+        cout << "Error on command write: " << ec.message() << endl;
+    }
+}
+
 void RaftRoboteq::start_read() {
     boost::asio::async_read_until(sock, input_buffer, '\n',
             boost::bind(&RaftRoboteq::handle_read, this, _1));
@@ -122,7 +140,9 @@ void RaftRoboteq::handle_read(const boost::system::error_code& ec) {
         // Extract the newline-delimited message from the buffer.
         std::string line;
         std::istream is(&input_buffer);
-        cout << is.rdbuf() << endl;
+        std::getline(is, line);
+
+        parseLine(line);
 
         start_read();
     } else {
@@ -131,15 +151,117 @@ void RaftRoboteq::handle_read(const boost::system::error_code& ec) {
 }
 
 void RaftRoboteq::start_write() {
-    boost::asio::async_write(sock, boost::asio::buffer("hello\n", 6),
+    string slow_query = slow_queries[slow_query_index] + "?P\n";
+    boost::asio::async_write(sock, boost::asio::buffer(slow_query, slow_query.size()),
             boost::bind(&RaftRoboteq::handle_write, this, _1));
+
+    slow_query_index++;
+    if (slow_query_index == slow_queries.size()) {
+        slow_query_index = 0;
+    }
 }
 
 void RaftRoboteq::handle_write(const boost::system::error_code& ec) {
     if (!ec) {
-        deadline_timer.expires_from_now(boost::posix_time::seconds(1));
+        deadline_timer.expires_from_now(boost::posix_time::milliseconds(20));
         deadline_timer.async_wait(boost::bind(&RaftRoboteq::start_write, this));
     } else {
-        cout << "Write on write: " << ec.message() << endl;
+        cout << "Error on query write: " << ec.message() << endl;
+    }
+}
+
+void RaftRoboteq::setThrust(int channel, double thrust) {
+    int power = thrust * 10.0;
+    char command [100];
+    int n = sprintf(command, "!G %d %d\n", channel, power);
+    boost::asio::async_write(sock, boost::asio::buffer(command, n),
+            boost::bind(&RaftRoboteq::handle_command_write, this, _1));
+}
+
+void RaftRoboteq::setArmPower(bool power) {
+    string command;
+    if (power)
+        command = "!D1 1\n";
+    else
+        command = "!D0 1\n";
+
+    boost::asio::async_write(sock, boost::asio::buffer(command, command.size()),
+            boost::bind(&RaftRoboteq::handle_eca_power_write, this, _1));
+}
+
+void RaftRoboteq::parseLine(string line) {
+    switch (line[0]) {
+    case 'V':
+        int v1, v2, v3;
+        sscanf(line.c_str(), "V=%d:%d:%d", &v1, &v2, &v3);
+        m_Comms.Notify("ROBOTEQ_BATTERY_VOLTAGE", v2/10.0);
+        break;
+
+    case 'A':
+        int a1, a2;
+        sscanf(line.c_str(), "A=%d:%d", &a1, &a2);
+        m_Comms.Notify("ROBOTEQ_MOTOR_CURRENT_LEFT", a1/10.0);
+        m_Comms.Notify("ROBOTEQ_MOTOR_CURRENT_RIGHT", a2/10.0);
+        break;
+
+    case 'B':
+        int b1;
+        sscanf(line.c_str(), "BA=%d", &b1);
+        m_Comms.Notify("ROBOTEQ_BATTERY_CURRENT", b1/10.0);
+        break;
+
+    case 'T':
+        int tm, t1, t2;
+        sscanf(line.c_str(), "T=%d,%d,%d", &tm, &t1, &t2);
+        m_Comms.Notify("ROBOTEQ_IC_TEMPERATURE", tm/10.0);
+        m_Comms.Notify("ROBOTEQ_TEMPERATURE_LEFT", t1/10.0);
+        m_Comms.Notify("ROBOTEQ_TEMPERATURE_RIGHT", t2/10.0);
+        break;
+
+    case 'D':
+        int d1;
+        sscanf(line.c_str(), "DI=%d", &d1);
+        m_Comms.Notify("ROBOTEQ_ESTOP", (double) d1);
+        break;
+
+    case 'P':
+        int p1, p2;
+        sscanf(line.c_str(), "P=%d,%d", &p1, &p2);
+        m_Comms.Notify("ROBOTEQ_POWER_LEFT", p1);
+        m_Comms.Notify("ROBOTEQ_POWER_RIGHT", p2);
+        power_report_count++;
+        break;
+
+    case '+':
+        power_ack_count++;
+        break;
+
+    case '-':
+        power_nack_count++;
+        break;
+    }
+
+
+    if (MOOSTime() - last_report_time > 5) {
+        double time_elapsed = MOOSTime() - last_report_time;
+        double power_report_rate = power_report_count / time_elapsed;
+        double power_command_rate = power_command_count / time_elapsed;
+
+        double power_nack_percent;
+        if (power_command_count == 0) {
+            power_nack_percent = 0;
+        } else {
+            power_nack_percent = 100.0 * power_nack_count / power_command_count;
+        }
+
+        m_Comms.Notify("ROBOTEQ_REPORT_RATE", power_report_rate);
+        m_Comms.Notify("ROBOTEQ_COMMAND_RATE", power_command_rate);
+        m_Comms.Notify("ROBOTEQ_NACK_PERCENT", power_nack_percent);
+
+        power_report_count = 0;
+        power_command_count = 0;
+        power_ack_count = 0;
+        power_nack_count = 0;
+        last_report_time = MOOSTime();
     }
 }
